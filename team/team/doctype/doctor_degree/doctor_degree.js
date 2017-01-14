1918ed0004ce6b641d5e6cc31c68a3a1cc936dfc
vin <vinupatil9@gmail.com>
2017-01-14 16:21:31 +0530

    vin commit

---
+++ team/team/doctype/doctor_degree/doctor_degree.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2016, Vinayak Patil and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Doctor Degree', {
+	refresh: function(frm) {
+
+	}
+});
