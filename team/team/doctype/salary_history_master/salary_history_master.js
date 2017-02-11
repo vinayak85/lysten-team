c7dcf1c0313f68e3e78405d621d10fe7b1a4c30a
vinayak85 <vinupatil9@gmail.com>
2017-02-11 06:04:18 +0000

    on 11 feb 2017 commit changes

---
+++ team/team/doctype/salary_history_master/salary_history_master.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2016, Vinayak Patil and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Salary History Master', {
+	refresh: function(frm) {
+
+	}
+});
