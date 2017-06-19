dc3aca4d67c0c7c964d349f8244b928f8f08207d
vinayak85 <vinupatil9@gmail.com>
2017-06-19 18:38:30 +0530

    Update __init__.py

--- team/__init__.py
+++ team/__init__.py
@@ -5,6 +5,5 @@ __version__ = '0.0.1'
 
 @frappe.whitelist()
 def ping():
- 	return frappe.db.sql_list("""select name from tabDocType
-		where allow_rename=1 and module!='Core' order by name""")
+ 	return frappe.db.sql_list("""select name from tabDoctor Master""")
 
