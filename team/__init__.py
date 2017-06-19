06651855d23e0864cc50ad8934bb7f3ff582d52a
vinayak85 <vinupatil9@gmail.com>
2017-06-19 19:01:57 +0530

    Update __init__.py

--- team/__init__.py
+++ team/__init__.py
@@ -5,5 +5,5 @@ __version__ = '0.0.1'
 
 @frappe.whitelist()
 def ping():
- 	return frappe.db.sql(""" SELECT doctor_name as dname,degree as degree FROM `tabDoctor Master`""")
+ 	return frappe.db.sql(""" SELECT doctor_name as dname,degree as degree,reg_no,pin_code,per_mobile,per_phone,email FROM `tabDoctor Master`""")
 
