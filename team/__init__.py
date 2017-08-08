6d255703e7a6ee79b71aef205233b7fd69fa480c
vinayak85 <vinupatil9@gmail.com>
2017-08-08 19:04:00 +0530

    Update __init__.py

--- team/__init__.py
+++ team/__init__.py
@@ -3,8 +3,8 @@ from __future__ import unicode_literals
 import frappe
 __version__ = '0.0.1'
 
-@frappe.whitelist()
+@frappe.whitelist(start, page_len)
 def ping():
  	return frappe.db.sql(""" SELECT doctor_name as dname,reg_no,pin_code,
-  per_mobile,per_phone,email FROM `tabDoctor Master` LIMIT 3 OFFSET 6;""", as_dict=True )
+  per_mobile,per_phone,email FROM `tabDoctor Master` limit %(start)s, %(page_len)s""", as_dict=True )
 
