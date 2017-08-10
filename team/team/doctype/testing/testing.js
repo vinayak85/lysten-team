a2c1c36889baa434fe109a2f160c36d62013ff8a
vinayak85 <vinupatil9@gmail.com>
2017-08-10 17:07:46 +0530

    Update testing.js

--- team/team/doctype/testing/testing.js
+++ team/team/doctype/testing/testing.js
@@ -6,3 +6,9 @@ frappe.ui.form.on('testing', {
 
 	}
 });
+frappe.ui.form.on("testing", "btn1", function(frm) {
+  frappe.call({
+    method: "team.team.doctype.testing.testing.ching",
+    callback: function(r) { };
+  });
+});
