c2157962871ea2ab8fc776b446119d656cf0b21b
vinayak85 <vinupatil9@gmail.com>
2017-08-10 18:07:13 +0530

    Update testing.js

--- team/team/doctype/testing/testing.js
+++ team/team/doctype/testing/testing.js
@@ -1,13 +1,14 @@
 // Copyright (c) 2017, Vinayak Patil and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on("testing", "btn1",
-    function(frm) {
-        frappe.call({
-	    alert("hi");
-            "method": "team.team.doctype.testing.testing.ching",
-            callback: function (data) {
-              console.log(data);                
-            }
-        })
-    });
+frappe.ui.form.on('testing', {
+	btn1: function(frm) {
+		alert("hi");
+			}
+		}
+	},
+
+	refresh: function(frm) {
+
+	},
+});
