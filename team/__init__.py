99230eb619ce7421005eb1b2da7489447d27091e
vinayak85 <vinupatil9@gmail.com>
2017-08-10 16:16:32 +0530

    Update __init__.py

--- team/__init__.py
+++ team/__init__.py
@@ -8,6 +8,7 @@ def ping(limit, offset):
  	return frappe.db.sql(""" SELECT doctor_name as dname,reg_no,pin_code,
   per_mobile,per_phone,email FROM `tabDoctor Master` LIMIT {0}  OFFSET {1} """.format(limit,offset),as_dict=True)
  
+ @frappe.whitelist()
  def pung(limit, offset):
  	return frappe.db.sql(""" SELECT doctor_name as dname,reg_no,pin_code,
   per_mobile,per_phone,email FROM `tabDoctor Master` LIMIT {0}  OFFSET {1} """.format(limit,offset),as_dict=True)
