d6ca95d90f942bf3436c63b8c43e2344388c71eb
vinayak85 <vinupatil9@gmail.com>
2017-08-10 18:01:32 +0530

    Update testing.js

--- team/team/doctype/testing/testing.js
+++ team/team/doctype/testing/testing.js
@@ -1,11 +1,6 @@
 // Copyright (c) 2017, Vinayak Patil and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('testing', {
-	refresh: function(frm) {
-
-	}
-});
 frappe.ui.form.on("testing", "btn1",
     function(frm) {
         frappe.call({
