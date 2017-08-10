6f322cc0fe24ac86422f7841f7f707c3bef6c3b8
vinayak85 <vinupatil9@gmail.com>
2017-08-10 13:09:02 +0530

    Update __init__.py

--- team/__init__.py
+++ team/__init__.py
@@ -6,5 +6,5 @@ __version__ = '0.0.1'
 @frappe.whitelist(limit, offset)
 def ping():
  	return frappe.db.sql(""" SELECT doctor_name as dname,reg_no,pin_code,
-  per_mobile,per_phone,email FROM `tabDoctor Master` LIMIT %s OFFSET %s""", (limit,offset))
+  per_mobile,per_phone,email FROM `tabDoctor Master` LIMIT  1 OFFSET 0""", (limit,offset))
 
