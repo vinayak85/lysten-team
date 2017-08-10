f4e6b78dd9bfe06ba38f5bf4457c348e8fd27c52
vinayak85 <vinupatil9@gmail.com>
2017-08-10 17:58:40 +0530

    Update testing.js

--- team/team/doctype/testing/testing.js
+++ team/team/doctype/testing/testing.js
@@ -9,6 +9,7 @@ frappe.ui.form.on('testing', {
 frappe.ui.form.on("testing", "btn1",
     function(frm) {
         frappe.call({
+	    alert("hi");
             "method": "team.team.doctype.testing.testing.ching",
             callback: function (data) {
               console.log(data);                
