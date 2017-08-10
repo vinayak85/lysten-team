cdf307c27a5759fcac12749124c5adfadf776494
vinayak85 <vinupatil9@gmail.com>
2017-08-10 16:39:05 +0530

    Update testing.py

--- team/team/doctype/testing/testing.py
+++ team/team/doctype/testing/testing.py
@@ -8,3 +8,7 @@ from frappe.model.document import Document
 
 class testing(Document):
 	pass
+@frappe.whitelist()
+def ching(limit, offset):
+ 	return frappe.db.sql(""" SELECT doctor_name as dname,reg_no,pin_code,
+  per_mobile,per_phone,email FROM `tabDoctor Master` LIMIT {0}  OFFSET {1} """.format(limit,offset),as_dict=True)
