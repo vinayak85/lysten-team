1f6d65d362afe8fe777a27e46607b20b2254f611
vinayak85 <vinupatil9@gmail.com>
2017-08-12 14:13:02 +0530

    Update __init__.py

--- team/__init__.py
+++ team/__init__.py
@@ -13,5 +13,7 @@ def ping(limit, offset):
 def ting(): 
  return frappe.db.sql(""" select name,username,full_name,first_name,middle_name,last_name,designation,mobile_no1,email,
  modified from 1bd3e0294da19198.`tabUser` 
- where `tabUser`.`enabled`=1 """,as_dict=True)
+ where `tabUser`.`enabled`=1  and `tabUser`.`name` in(
+ (select abm from 1bd3e0294da19198.`tabUser` where `name`='riteshdiwan8@gmail.com')
+ )""",as_dict=True)
 
