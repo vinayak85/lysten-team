d9e96669e309aaaf07dfb72d48eb70995ce2b3e8
vinayak85 <vinupatil9@gmail.com>
2017-08-12 15:51:19 +0530

    Update __init__.py

--- team/__init__.py
+++ team/__init__.py
@@ -33,5 +33,43 @@ def ting(employee, designation):
  ,(select sm from 1bd3e0294da19198.`tabUser` where `name`={0})
  ,(select nbm from 1bd3e0294da19198.`tabUser` where `name`={0})
  )""".format(employee),as_dict=True)
+ elif designation == "RBM":
+  return frappe.db.sql(""" select name,username,full_name,first_name,middle_name,last_name,designation,mobile_no1,email,
+ modified from 1bd3e0294da19198.`tabUser` 
+ where `tabUser`.`enabled`=1 and `tabUser`.`rbm`={0}  or `tabUser`.`name` in(
+ (select zbm from 1bd3e0294da19198.`tabUser` where `name`={0})
+ ,(select crm from 1bd3e0294da19198.`tabUser` where `name`={0})
+ ,(select sm from 1bd3e0294da19198.`tabUser` where `name`={0})
+ ,(select nbm from 1bd3e0294da19198.`tabUser` where `name`={0})
+ )""".format(employee),as_dict=True)
+ elif designation == "ZBM":
+  return frappe.db.sql(""" select name,username,full_name,first_name,middle_name,last_name,designation,mobile_no1,email,
+ modified from 1bd3e0294da19198.`tabUser` 
+ where `tabUser`.`enabled`=1 and `tabUser`.`zbm`={0}  or `tabUser`.`name` in(
+ (select crm from 1bd3e0294da19198.`tabUser` where `name`={0})
+ ,(select sm from 1bd3e0294da19198.`tabUser` where `name`={0})
+ ,(select nbm from 1bd3e0294da19198.`tabUser` where `name`={0})
+ )""".format(employee),as_dict=True)
+ elif designation == "SM":
+  return frappe.db.sql(""" select name,username,full_name,first_name,middle_name,last_name,designation,mobile_no1,email,
+ modified from 1bd3e0294da19198.`tabUser` 
+ where `tabUser`.`enabled`=1 and `tabUser`.`sm`={0}  or `tabUser`.`name` in(
+ (select crm from 1bd3e0294da19198.`tabUser` where `name`={0})
+ ,(select nbm from 1bd3e0294da19198.`tabUser` where `name`={0})
+ )""".format(employee),as_dict=True)
+ elif designation == "NBM":
+  return frappe.db.sql(""" select name,username,full_name,first_name,middle_name,last_name,designation,mobile_no1,email,
+ modified from 1bd3e0294da19198.`tabUser` 
+ where `tabUser`.`enabled`=1 and `tabUser`.`nbm`={0}  or `tabUser`.`name` in(
+ (select crm from 1bd3e0294da19198.`tabUser` where `name`={0})
+ )""".format(employee),as_dict=True)
+ elif designation == "CRM":
+  return frappe.db.sql(""" select name,username,full_name,first_name,middle_name,last_name,designation,mobile_no1,email,
+ modified from 1bd3e0294da19198.`tabUser` 
+ where `tabUser`.`enabled`=1 and `tabUser`.`crm`={0}""".format(employee),as_dict=True)
+ elif (designation == "HR Manager" or designation == "Head of Marketing and Sales"):
+  return frappe.db.sql(""" select name,username,full_name,first_name,middle_name,last_name,designation,mobile_no1,email,
+ modified from 1bd3e0294da19198.`tabUser` 
+ where `tabUser`.`enabled`=1 and `tabUser`.`name` not in('vinupatil9@gmail.com','arjunapatil08.ap@gmail.com')""".format(employee),as_dict=True)
  else:
    frappe.msgprint(_("NN"))
