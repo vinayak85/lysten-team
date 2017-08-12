3b6e72e8baafb0caa3c1424b76e7f3ba3f8bb2be
vinayak85 <vinupatil9@gmail.com>
2017-08-12 14:27:33 +0530

    Update __init__.py

--- team/__init__.py
+++ team/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 import frappe
-from frappe import msgprint, 
+from frappe import msgprint, _ 
 __version__ = '0.0.1'
 
 @frappe.whitelist()
