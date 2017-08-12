ee196da61100361fc1f159eaa897914c729658f0
vinayak85 <vinupatil9@gmail.com>
2017-08-12 14:30:38 +0530

    Update __init__.py

--- team/__init__.py
+++ team/__init__.py
@@ -13,6 +13,10 @@ def ping(limit, offset):
 @frappe.whitelist()
 def ting(employee, designation):
  if designation == "TBM":
-   frappe.msgprint(_("TT"))
+   return frappe.db.sql(""" select name,username,full_name,first_name,middle_name,last_name,designation,mobile_no1,email,
+ modified from 1bd3e0294da19198.`tabUser` 
+ where `tabUser`.`enabled`=1  and `tabUser`.`name` in(
+ (select abm from 1bd3e0294da19198.`tabUser` where `name`='riteshdiwan8@gmail.com')
+ )""",as_dict=True)
  else:
    frappe.msgprint(_("NN"))
