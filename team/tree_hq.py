edb22895493a5f2b11409900e621bace0f04b088
vinayak85 <vinupatil9@gmail.com>
2017-08-17 18:13:57 +0530

    Update tree_hq.py

--- team/tree_hq.py
+++ team/tree_hq.py
@@ -8,7 +8,6 @@ __version__ = '0.0.1'
 @frappe.whitelist()
 def tree_territory(employee, designation,limit, offset):
  if designation == "'ABM'":
- return frappe.db.sql(""" SELECT c1.`name`,c1.`territory_name`,c1.`parent_territory` FROM 1bd3e0294da19198.`tabTerritory` AS c1 JOIN 1bd3e0294da19198.tabTerritory  AS c2 ON (c2.territory_name = c1.parent_territory)
-   JOIN 1bd3e0294da19198.`tabTerritory`  AS c3 ON (c3.`territory_name` = c2.`parent_territory`)JOIN 1bd3e0294da19198.`tabTerritory`  AS c4 ON (c4.`territory_name` = c3.`parent_territory`)
-   JOIN 1bd3e0294da19198.`tabTerritory`  AS c5 ON (c5.`territory_name` = c4.`parent_territory`)
-   where c1.`parent_territory`={0} LIMIT {1}  OFFSET {2} """.format(employee,limit,offset),as_dict=True)
+      frappe.msgprint(_("abm"))
+   else
+   frappe.msgprint(_("tbm"))
