b4cea3eb50afbcd26061219d67fe44f9180513eb
vinayak85 <vinupatil9@gmail.com>
2017-08-17 18:20:01 +0530

    Update tree_hq.py

--- team/tree_hq.py
+++ team/tree_hq.py
@@ -1,13 +1,10 @@
+# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 import frappe
 from frappe import msgprint, _ 
 __version__ = '0.0.1'
 
-# this method is used for android heirachy user
- #it will featch all top and down users of selected user
 @frappe.whitelist()
-def tree_territory(employee, designation,limit, offset):
- if designation == "'ABM'":
-      frappe.msgprint(_("abm"))
-   else
-   frappe.msgprint(_("tbm"))
+def tttt(limit, offset):
+ 	return frappe.db.sql(""" SELECT doctor_name as dname,reg_no,pin_code,
+  per_mobile,per_phone,email FROM `tabDoctor Master` LIMIT {0}  OFFSET {1} """.format(limit,offset),as_dict=True)
