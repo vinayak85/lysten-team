3e222d8bce21dcdb36741c65048e7c92360f4d8d
vinayak85 <vinupatil9@gmail.com>
2017-08-17 15:52:24 +0530

    Update tree.py

--- team/tree.py
+++ team/tree.py
@@ -83,7 +83,7 @@ def tree_user(employee, designation,limit, offset):
 def tree_territory(territory,designation,limit, offset):
  return frappe.db.sql(""" SELECT 	c1.name as 'headquarter_id', c1.territory_name as 'headquarter_name',
                            c1.parent_territory as 'headquarter_parent'
-                           FROM 1bd3e0294da19198.tabTerritory  AS c1
+                           FROM 1bd3e0294da19198.tabTerritory LIMIT {1}  OFFSET {2} AS c1
                            JOIN 1bd3e0294da19198.tabTerritory  AS c2 ON (c2.territory_name = c1.parent_territory)
                            JOIN 1bd3e0294da19198.tabTerritory  AS c3 ON (c3.territory_name = c2.parent_territory)
                            JOIN 1bd3e0294da19198.tabTerritory  AS c4 ON (c4.territory_name = c3.parent_territory)
