87f1a6f4b729191084f0b0d5370d168b2d1b1a60
vinayak85 <vinupatil9@gmail.com>
2017-08-17 16:12:01 +0530

    Update tree.py

--- team/tree.py
+++ team/tree.py
@@ -81,7 +81,6 @@ def tree_user(employee, designation,limit, offset):
    
 @frappe.whitelist()
 def tree_territory(territory,designation,limit, offset):
-if designation == "ABM":
  return frappe.db.sql(""" SELECT 	c1.name as 'headquarter_id', c1.territory_name as 'headquarter_name',
                            c1.parent_territory as 'headquarter_parent'
                            FROM 1bd3e0294da19198.tabTerritory AS c1 
