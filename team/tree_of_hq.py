111dc8bb831d63deee224ba9e4f1585a83d24c05
vinayak85 <vinupatil9@gmail.com>
2017-08-17 17:03:48 +0530

    Update tree_of_hq.py

--- team/tree_of_hq.py
+++ team/tree_of_hq.py
@@ -5,7 +5,7 @@ from frappe import msgprint, _
 
 @frappe.whitelist()
 def tree_territory(a,b,limit, offset): 
- if b == "ABM":
+ if (b == "ABM"):
  return frappe.db.sql(""" SELECT 	c1.name as 'headquarter_id', c1.territory_name as 'headquarter_name',
                            c1.parent_territory as 'headquarter_parent'
                            FROM 1bd3e0294da19198.tabTerritory AS c1 
