ca47acd179f6c5a006cb26453fdd4db3206b7af6
vinayak85 <vinupatil9@gmail.com>
2017-08-17 14:38:01 +0530

    Update test.py

--- team/test.py
+++ team/test.py
@@ -8,3 +8,9 @@ __version__ = '0.0.1'
 def tooo(limit, offset):
  	return frappe.db.sql(""" SELECT doctor_name as dname,reg_no,pin_code,
   per_mobile,per_phone,email FROM `tabDoctor Master` LIMIT {0}  OFFSET {1} """.format(limit,offset),as_dict=True)
+ 
+ 
+ @frappe.whitelist()
+def po(limit, offset):
+ 	return frappe.db.sql(""" SELECT doctor_name as dname,reg_no,pin_code,
+  per_mobile,per_phone,email FROM `tabDoctor Master` LIMIT {0}  OFFSET {1} """.format(limit,offset),as_dict=True)
