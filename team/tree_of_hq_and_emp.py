c6f480d70d94957ca68d2fc91a8fb5f5e5189075
vinayak85 <vinupatil9@gmail.com>
2017-08-17 14:19:35 +0530

    Update tree_of_hq_and_emp.py

--- team/tree_of_hq_and_emp.py
+++ team/tree_of_hq_and_emp.py
@@ -74,7 +74,7 @@ def tree_user(employee, designation,limit, offset):
  where `tabUser`.`enabled`=1 and `tabUser`.`crm`={0}
  LIMIT {1}  OFFSET {2} """.format(employee,limit,offset),as_dict=True)
  
- elif (designation == "HR Manager" or designation == "Head of Marketing and Sales"):
+ elif (designation == "HR Manager" or designation == "Head of Marketing and Sales" or designation == "Admin"):
   return frappe.db.sql(""" select name,username,full_name,first_name,middle_name,last_name,designation,mobile_no1,email,
  modified from 1bd3e0294da19198.`tabUser` 
  where `tabUser`.`enabled`=1 and `tabUser`.`designation` in('TBM','ABM','RBM','ZBM','SM','NBM','CRM')
@@ -82,4 +82,67 @@ def tree_user(employee, designation,limit, offset):
  
  else:
    frappe.msgprint(_("No entry"))
+   
+  
 
+  
+  # this method is used for android heirachy get Hq based on user territory
+ #it will featch all HQ of related to the user
+@frappe.whitelist()
+
+def tree_user(territory, designation,limit, offset):
+ 
+if designation == "ABM":
+  return frappe.db.sql(""" SELECT 	c1.name as 'headquarter_id', c1.territory_name as 'headquarter_name',
+                           c1.parent_territory as 'headquarter_parent'
+                           FROM 1bd3e0294da19198.tabTerritory  AS c1
+                           JOIN 1bd3e0294da19198.tabTerritory  AS c2 ON (c2.territory_name = c1.parent_territory)
+                           JOIN 1bd3e0294da19198.tabTerritory  AS c3 ON (c3.territory_name = c2.parent_territory)
+                           JOIN 1bd3e0294da19198.tabTerritory  AS c4 ON (c4.territory_name = c3.parent_territory)
+                           JOIN 1bd3e0294da19198.tabTerritory  AS c5 ON (c5.territory_name = c4.parent_territory)
+                           where 
+                           c1.parent_territory={0};
+                           """.format(territory,limit,offset),as_dict=True)
+ 
+if designation == "RBM":
+  return frappe.db.sql(""" SELECT 	c1.name as 'headquarter_id', c1.territory_name as 'headquarter_name',
+                           c1.parent_territory as 'headquarter_parent'
+                           FROM 1bd3e0294da19198.tabTerritory  AS c1
+                           JOIN 1bd3e0294da19198.tabTerritory  AS c2 ON (c2.territory_name = c1.parent_territory)
+                           JOIN 1bd3e0294da19198.tabTerritory  AS c3 ON (c3.territory_name = c2.parent_territory)
+                           JOIN 1bd3e0294da19198.tabTerritory  AS c4 ON (c4.territory_name = c3.parent_territory)
+                           JOIN 1bd3e0294da19198.tabTerritory  AS c5 ON (c5.territory_name = c4.parent_territory)
+                           where 
+                           c2.parent_territory={0};
+                           """.format(territory,limit,offset),as_dict=True)
+ 
+ if designation == "ZBM":
+  return frappe.db.sql(""" SELECT 	c1.name as 'headquarter_id', c1.territory_name as 'headquarter_name',
+                           c1.parent_territory as 'headquarter_parent'
+                           FROM 1bd3e0294da19198.tabTerritory  AS c1
+                           JOIN 1bd3e0294da19198.tabTerritory  AS c2 ON (c2.territory_name = c1.parent_territory)
+                           JOIN 1bd3e0294da19198.tabTerritory  AS c3 ON (c3.territory_name = c2.parent_territory)
+                           JOIN 1bd3e0294da19198.tabTerritory  AS c4 ON (c4.territory_name = c3.parent_territory)
+                           JOIN 1bd3e0294da19198.tabTerritory  AS c5 ON (c5.territory_name = c4.parent_territory)
+                           where 
+                           c3.parent_territory={0};
+                           """.format(territory,limit,offset),as_dict=True) 
+ 
+
+ 
+ elif (designation == "HR Manager" or designation == "Head of Marketing and Sales"
+       or designation == "SM" or designation == "CRM" or designation == "NBM"
+       or designation == "Admin"):
+ return frappe.db.sql(""" SELECT 	c1.name as 'headquarter_id', c1.territory_name as 'headquarter_name',
+                           c1.parent_territory as 'headquarter_parent'
+                           FROM 1bd3e0294da19198.tabTerritory  AS c1
+                           JOIN 1bd3e0294da19198.tabTerritory  AS c2 ON (c2.territory_name = c1.parent_territory)
+                           JOIN 1bd3e0294da19198.tabTerritory  AS c3 ON (c3.territory_name = c2.parent_territory)
+                           JOIN 1bd3e0294da19198.tabTerritory  AS c4 ON (c4.territory_name = c3.parent_territory)
+                           JOIN 1bd3e0294da19198.tabTerritory  AS c5 ON (c5.territory_name = c4.parent_territory)
+                           where 
+                           c4.parent_territory={0};
+                           """.format(territory,limit,offset),as_dict=True) 
+ 
+ else:
+   frappe.msgprint(_("No entry"))
