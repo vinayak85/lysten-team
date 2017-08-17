30c30099f5fb2779d3c902f05ed371596394517c
vinayak85 <vinupatil9@gmail.com>
2017-08-17 16:52:06 +0530

    Update tree_of_hq.py

--- team/tree_of_hq.py
+++ team/tree_of_hq.py
@@ -1,8 +1,8 @@
 from __future__ import unicode_literals
 import frappe
 from frappe import msgprint, _ 
-__version__ = '0.0.1'
+
 
 @frappe.whitelist()
-def tree_territory(territory,limit, offset):
+def tree_territory(aaaa,limit, offset):
  frappe.msgprint(_("No entry"))
