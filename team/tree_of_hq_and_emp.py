000e57aa3f1c80c93d1e0f69844bea5d148bcf01
vinayak85 <vinupatil9@gmail.com>
2017-08-17 14:03:23 +0530

    Update and rename tree_of_hq_and_emp to tree_of_hq_and_emp.py

--- team/tree_of_hq_and_emp.py
+++ team/tree_of_hq_and_emp.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+import frappe
+from frappe import msgprint, _ 
+__version__ = '0.0.1'
+
+
 # this method is used for android heirachy user
  #it will featch all top and down users of selected user
 @frappe.whitelist()
