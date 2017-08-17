dbafa8bb6351757aa5c0138d391d1c0a9526f337
vinayak85 <vinupatil9@gmail.com>
2017-08-17 17:30:08 +0530

    Update tree_of_hq.py

--- team/tree_of_hq.py
+++ team/tree_of_hq.py
@@ -5,13 +5,4 @@ from frappe import msgprint, _
 
 @frappe.whitelist()
 def tree_territory(b,limit, offset):
- return frappe.db.sql(""" SELECT c1.name as 'headquarter_id', c1.territory_name as 'headquarter_name',
-                           c1.parent_territory as 'headquarter_parent'
-                           FROM 1bd3e0294da19198.tabTerritory AS c1 
-                           JOIN 1bd3e0294da19198.tabTerritory  AS c2 ON (c2.territory_name = c1.parent_territory)
-                           JOIN 1bd3e0294da19198.tabTerritory  AS c3 ON (c3.territory_name = c2.parent_territory)
-                           JOIN 1bd3e0294da19198.tabTerritory  AS c4 ON (c4.territory_name = c3.parent_territory)
-                           JOIN 1bd3e0294da19198.tabTerritory  AS c5 ON (c5.territory_name = c4.parent_territory)
-                           where 
-                           c1.parent_territory='Kolhapur(Area)' 
-                           """.format(b,limit,offset),as_dict=True)
+  frappe.msgprint(_(b))
