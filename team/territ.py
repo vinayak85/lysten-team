30d3515ef8024ba702cb465b66a92ef6530dc139
vinayak85 <vinupatil9@gmail.com>
2017-08-18 18:45:29 +0530

    Update territ.py

--- team/territ.py
+++ team/territ.py
@@ -6,9 +6,9 @@ __version__ = '0.0.1'
 
 
 @frappe.whitelist()
-def tttt(tt): 
-if tt == 'TBM':
- return frappe.db.sql(""" SELECT 	c1.name as 'headquarter_id', c1.territory_name as 'headquarter_name',
+def tttt(tt):
+ if tt == 'TBM':
+  return frappe.db.sql(""" SELECT 	c1.name as 'headquarter_id', c1.territory_name as 'headquarter_name',
                            c1.parent_territory as 'headquarter_parent'
                            FROM 1bd3e0294da19198.tabTerritory  AS c1
                            JOIN 1bd3e0294da19198.tabTerritory  AS c2 ON (c2.territory_name = c1.parent_territory)
@@ -17,5 +17,5 @@ if tt == 'TBM':
                            JOIN 1bd3e0294da19198.tabTerritory  AS c5 ON (c5.territory_name = c4.parent_territory)
                            ;                          
                            """, as_dict=True)
-else:
- frappe.msgprint(_("No entry"))
+ else:
+  frappe.msgprint(_("No entry"))
