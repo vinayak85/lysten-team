de2773897256da13e37a88042325eeedf8992f06
vinayak85 <vinupatil9@gmail.com>
2017-08-18 18:22:33 +0530

    Update territ.py

--- team/territ.py
+++ team/territ.py
@@ -18,6 +18,6 @@ def tttt(tt):
                            JOIN 1bd3e0294da19198.tabTerritory  AS c4 ON (c4.territory_name = c3.parent_territory)
                            JOIN 1bd3e0294da19198.tabTerritory  AS c5 ON (c5.territory_name = c4.parent_territory)
                            ;                          
-                           """, as_dict=True) 
-    else: 
+                           """, as_dict=True)
+  else: 
       frappe.msgprint(_("No entry"))
