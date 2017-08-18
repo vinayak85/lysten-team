c1b8f826bb7559d9ec32b3b8d9281c185aae33b1
ArjunaPatil <30894586+ArjunaPatil@users.noreply.github.com>
2017-08-18 15:10:59 +0530

    Update terit.py

--- team/terit.py
+++ team/terit.py
@@ -5,8 +5,11 @@ from frappe import msgprint, _
 __version__ = '0.0.1'
 
 @frappe.whitelist()
-def ping(limit, offset):
+def ding(limit, offset):
+ if limit==10
+ frappe.msgprint(_("aaaaaaaa"))
+ 
+ @frappe.whitelist()
+def dong(limit, offset):
  	return frappe.db.sql(""" SELECT doctor_name as dname,reg_no,pin_code,
   per_mobile,per_phone,email FROM `tabDoctor Master` LIMIT {0}  OFFSET {1} """.format(limit,offset),as_dict=True)
-
- 
