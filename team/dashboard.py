f12bfdface96492d74296ab15fcdd7a2f1579fad
vinayak85 <vinupatil9@gmail.com>
2017-08-21 15:08:26 +0530

    Update dashboard.py

--- team/dashboard.py
+++ team/dashboard.py
@@ -3,14 +3,14 @@ import frappe
 from frappe import msgprint, _ 
 __version__ = '0.0.1'
 
-
+@frappe.whitelist()
 def get_count_of_objectives_of_bottom_emp(employee, designation):
  frappe.msgprint(_(tree_user_bottom(employee, designation)))
 
 
 # this method is used for android heirachy user
  #it will featch all top and down users of selected user
-@frappe.whitelist()
+
 def tree_user_bottom(employee, designation): 
  if designation == 'TBM':
    return ""
