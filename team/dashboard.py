0d6370404d32acfa9ddab9ed2fd2f99b115111bb
vinayak85 <vinupatil9@gmail.com>
2017-08-21 17:24:00 +0530

    Update dashboard.py

--- team/dashboard.py
+++ team/dashboard.py
@@ -20,11 +20,14 @@ def get_count_of_objectives_of_bottom_emp(employee, designation):
  email_list=email_list[:-1]
  
  #return email_list
+ qry="SELECT count(*) as cnt_ob FROM 1bd3e0294da19198.tabObjective where 1bd3e0294da19198.tabObjective.select_date="
+ +today_date+" and 1bd3e0294da19198.tabObjective.user in"+email_list+""
+
  count_of_emp_objective= frappe.db.sql("""SELECT count(*) as cnt_ob FROM 1bd3e0294da19198.tabObjective
 where 1bd3e0294da19198.tabObjective.select_date={0} and
 1bd3e0294da19198.tabObjective.user in %s""".format(today_date),email_list, as_dict=1)
  
- return (today_date,count_of_emp,email_list,count_of_emp_objective)
+ return (today_date,count_of_emp,email_list,count_of_emp_objective,qry)
 
 
 # this method is used for android heirachy user
