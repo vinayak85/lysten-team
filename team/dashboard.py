bcf774b0a7cd60621f9bcbb3e3c509f6f4173a4b
vinayak85 <vinupatil9@gmail.com>
2017-08-21 15:38:55 +0530

    Update dashboard.py

--- team/dashboard.py
+++ team/dashboard.py
@@ -9,7 +9,7 @@ def get_count_of_objectives_of_bottom_emp(employee, designation):
  #return tree_user_bottom(employee, designation)
  email_list=""
  for email_emp in tree_user_bottom(employee, designation):
-  email_list = email_list + email_emp.name
+  email_list = email_list + email_emp.name + ","
  
  return email_list
  
