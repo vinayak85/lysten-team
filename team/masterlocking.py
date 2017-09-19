80d252d0d99577d5d91bbedc4796376fe23ea38c
vinayak85 <vinupatil9@gmail.com>
2017-09-19 16:01:56 +0530

    Update masterlocking.py

--- team/masterlocking.py
+++ team/masterlocking.py
@@ -67,7 +67,7 @@ def lock_transaction_forms(employee,formname,date):
             #import datetime
             time_diff = datetime.datetime.strptime(locktime, '%H:%M:%S') - datetime.datetime.strptime(current_time, '%H:%M:%S')
             minutes = int(time_diff.total_seconds()/60)
-            if minutes >= 0
+            if minutes >= 0:
             #if datetime.datetime(*time.strptime(current_time, '%H:%M:%S')).time() <= datetime.datetime(*time.strptime(locktime, '%H:%M:%S')).time():
             #if current_time <= locktime: 
                 frappe.msgprint(_(minutes))
