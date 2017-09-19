f663888064b28373d6ffce8dd5994c9812ad614c
vinayak85 <vinupatil9@gmail.com>
2017-09-19 15:35:03 +0530

    Update masterlocking.py

--- team/masterlocking.py
+++ team/masterlocking.py
@@ -57,6 +57,7 @@ def lock_transaction_forms(employee,formname,date):
     frmdate=dataarray[0].frm_date
     todate=dataarray[0].to_date
     locktime=dataarray[0].lock_time
+    locktime = locktime[:locktime.find('.') + 1]
     #frappe.msgprint(_(locktime))
     #frappe.msgprint(_(frmdate+todate+date))
     frappe.msgprint(_(current_time+' '+locktime))
