9b0404a8ade77a4fb115ad08e0b5ecedc245dd32
vinayak85 <vinupatil9@gmail.com>
2017-09-21 12:49:37 +0530

    Update masterlocking.py

--- team/masterlocking.py
+++ team/masterlocking.py
@@ -132,7 +132,7 @@ def lock_transaction_forms(employee,formname,date):
                         msg='Oops !!! Doctor Call Request For Today; Time is Over...'
                         
                     elif a=='c':
-                        msg=str(locktime)#'Oops !!! Chemist Call Request Request For Today; Time is Over...'
+                        msg='Oops !!! Chemist Call Request Request For Today; Time is Over...'
                         
                     elif a=='d':
                         msg='Oops !!! Campaign Booking Request For Today; Time is Over...'
@@ -203,7 +203,7 @@ def lock_transaction_forms(employee,formname,date):
                         msg='Oops !!! Doctor Call Request For Today; Time is Over...'
                         
                     elif a=='c':
-                        msg='Oops !!! Chemist Call Request Request For Today; Time is Over...'
+                        msg=str(locktime)#'Oops !!! Chemist Call Request Request For Today; Time is Over...'
                         
                     elif a=='d':
                         msg='Oops !!! Campaign Booking Request For Today; Time is Over...'
