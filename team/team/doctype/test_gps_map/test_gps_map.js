2c5650a20eb39040381b7d204f0154c7a40768cc
vinayak85 <vinupatil9@gmail.com>
2017-09-22 08:00:56 +0000

    comment

---
+++ team/team/doctype/test_gps_map/test_gps_map.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2017, Vinayak Patil and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('test_gps_map', {
+	refresh: function(frm) {
+
+	}
+});
