1ce2ab1cd2219428077cb667fb6f2f87c9564a06
vinayak85 <vinupatil9@gmail.com>
2017-10-23 11:20:35 +0530

    Update test.py

--- team/test.py
+++ team/test.py
@@ -27,7 +27,8 @@ def get_plan_of_today(employee, designation,date_pass,app_ver):
  if len(objective) > 0:
   if objective[0].dc==1 and objective[0].cm==1 and objective[0].mt==1 and objective[0].lv==0:
     objj="";
-    objj='\033[1m' + 'This is my text string.' + '\033[0m';#'Hiii';
+    objj='This is my \n text string.';
+    #'\033[1m' + 'This is my text string.' + '\033[0m';#'Hiii';
     #"<b>PLAN OF DAY :</b>"+ " DCR  |  CAMP BOOKING  |  MEETING \n\n"+"<b>DCR Agenda: </b>" + objective[0].dc_a +"\n"+"<b>CAMP Agenda: </b>" +objective[0].cm_a +"\n"+"<b>Meeting Agenda: </b>" +objective[0].dc_mt;
           
   elif objective[0].dc==1 and objective[0].cm==1 and objective[0].mt==0 and objective[0].lv==0:
