acb4c0c7007a45adcc3f32726e5c10bf443de864
vinayak85 <vinupatil9@gmail.com>
2017-11-14 17:52:22 +0530

    Update doctor_count_active_camp.py

--- team/doctor_count_active_camp.py
+++ team/doctor_count_active_camp.py
@@ -32,7 +32,7 @@ def count_active_camp_doctors(employee):
     camp=active_doc[0].camp_doc
     
     
-    if(active <= active1 and camp <= camp):
+    if(active < active1 and camp < camp):
         flag_action=1
         #flag_active='1'
         #flag_camp='1'
