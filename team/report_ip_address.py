466b70f89eb8ffda0b4c3b830e66e291fe226db6
vinayak85 <vinupatil9@gmail.com>
2017-11-16 11:34:38 +0530

    Update report_ip_address.py

--- team/report_ip_address.py
+++ team/report_ip_address.py
@@ -1,1 +1,14 @@
+from __future__ import unicode_literals
+import frappe
+import datetime
+from datetime import datetime
+from pytz import timezone
+from frappe import msgprint, _
+__version__ = '0.0.1'
 
+@frappe.whitelist()
+def ip_set_for_cry_report():
+    ip='13.127.22.180';
+    dict = {'ip': ''
+           }    
+    return dict
