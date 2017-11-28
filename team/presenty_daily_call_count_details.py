84e7e0998e0bb004955047d6f9dcec59731809a6
vinayak85 <vinupatil9@gmail.com>
2017-11-28 16:04:25 +0530

    Update presenty_daily_call_count_details.py

--- team/presenty_daily_call_count_details.py
+++ team/presenty_daily_call_count_details.py
@@ -51,7 +51,6 @@ def presenty_daily_call_count(fromdate,todate, designation):
 
 ###################
 ###################
-@frappe.whitelist()
 def missing_daily_obj_names(date):
   if((len(date)) == 0):
     date = frappe.utils.data.get_datetime().strftime('%Y/%m/%d')   
@@ -65,5 +64,5 @@ def missing_daily_obj_names(date):
    return frappe.db.sql(""" select concat(first_name,' ',last_name,' (',designation,' )')as emp_name,designation,name as email,
    ifnull(mobile_no1,'-') as mobno
    from 1bd3e0294da19198.tabUser where enabled=1 and designation in('TBM','ABM','RBM','SM','NBM') 
-   and name not in (select user from 1bd3e0294da19198.tabObjective where select_date='2017-11-28');""".format(date),as_dict=True)
+   and name not in (select user from 1bd3e0294da19198.tabObjective where select_date={0});""".format(date),as_dict=True)
   
