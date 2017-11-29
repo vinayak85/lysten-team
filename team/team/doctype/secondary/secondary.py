8ff553a8fc1ac93f59894212428e1a73f1d47a2f
vinayak85 <vinupatil9@gmail.com>
2017-11-29 17:06:33 +0530

    Update secondary.py

--- team/team/doctype/secondary/secondary.py
+++ team/team/doctype/secondary/secondary.py
@@ -22,7 +22,7 @@ def make_stock_entry(year, month,stockist,name):
 	
 @frappe.whitelist()
 def get_items(**args):
-	frappe.msgprint(_(args['filters']));
+	frappe.msgprint(_(frappe.get_list('Item',filters=args['filters'], fields=['name', 'item_name'])));
 	#return frappe.get_list('Item',filters=args['filters'], fields=['name', 'item_name'])	  
 
 
