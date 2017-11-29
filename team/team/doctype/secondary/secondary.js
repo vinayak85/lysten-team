bbd4410987058e602df3ed550568b16629a91041
vinayak85 <vinupatil9@gmail.com>
2017-11-29 18:45:37 +0530

    Update secondary.js

--- team/team/doctype/secondary/secondary.js
+++ team/team/doctype/secondary/secondary.js
@@ -7,8 +7,8 @@ frappe.ui.form.on('Secondary', {
 		frm.add_custom_button(__("Recalculate Secondary"),
 			function() { frm.trigger('get_items_all'); }, "fa fa-sitemap", "btn-default");
 		
-		frm.add_custom_button(__("test"),
-			function() { frm.trigger('test'); }, "fa fa-sitemap", "btn-default");
+		/*frm.add_custom_button(__("test"),
+			function() { frm.trigger('test'); }, "fa fa-sitemap", "btn-default");*/
 
 	},
 	
@@ -24,11 +24,15 @@ frappe.ui.form.on('Secondary', {
 				var sec_items_qty = $.map(frm.doc.sec_items_qty, function(d) { return d.sec_item_qty });
 				//alert(sec_items_qty );
 				for (var i=0; i< r.message.length; i++) {
+					if(test(frm,r.message[i].item_name)==true)
+					{
 					if (sec_items_qty.indexOf(r.message[i].name) === -1) {
 						var row = frappe.model.add_child(frm.doc, frm.fields_dict.sec_items_qty.df.options, frm.fields_dict.sec_items_qty.df.fieldname);
 						row.item_code = r.message[i].name;
 						row.item_code = r.message[i].item_name;
 						
+						
+					}
 					}
 				}
 				frm.refresh_field('sec_items_qty');
@@ -45,11 +49,13 @@ frappe.ui.form.on('Secondary', {
 		var tbl1 = frm.doc.sec_items_qty || [];
 		var strr="";
 		//var total_earn = 0; var total_ded = 0;
-	        for(var i = 0; i < 3; i++)
+	        for(var i = 0; i < tbl1.length; i++)
 		{
-		  strr=strr+" ... "+ tbl1[i].item_code;
+			if(check_item_name==tbl1[i].item_code)
+				return false;
+		  //strr=strr+" ... "+ tbl1[i].item_code;
 	        }
-		alert(strr);
+		
 		
 		
 	},
