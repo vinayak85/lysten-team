f55fb093076475565db79816281cfde504ac3ba9
vinayak85 <vinupatil9@gmail.com>
2017-11-29 18:49:11 +0530

    Update secondary.js

--- team/team/doctype/secondary/secondary.js
+++ team/team/doctype/secondary/secondary.js
@@ -24,7 +24,7 @@ frappe.ui.form.on('Secondary', {
 				var sec_items_qty = $.map(frm.doc.sec_items_qty, function(d) { return d.sec_item_qty });
 				//alert(sec_items_qty );
 				for (var i=0; i< r.message.length; i++) {
-					if(test(frm,r.message[i].item_name != false)
+					if(test(frm,r.message[i].item_name) != false)
 					{
 					if (sec_items_qty.indexOf(r.message[i].name) === -1) {
 						var row = frappe.model.add_child(frm.doc, frm.fields_dict.sec_items_qty.df.options, frm.fields_dict.sec_items_qty.df.fieldname);
