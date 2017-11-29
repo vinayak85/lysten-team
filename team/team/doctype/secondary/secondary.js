b6a8db1a4780d97c5a3134ddb1d57df69d472a4a
vinayak85 <vinupatil9@gmail.com>
2017-11-29 17:24:24 +0530

    Update secondary.js

--- team/team/doctype/secondary/secondary.js
+++ team/team/doctype/secondary/secondary.js
@@ -17,7 +17,17 @@ frappe.ui.form.on('Secondary', {
 				filters: filters
 			},
 			callback:function (r) {
-				alert("hello");
+				//alert("hello");
+				var sec_item_qty = $.map(frm.doc.sec_item_qty, function(d) { return d.sec_item });
+				for (var i=0; i< r.message.length; i++) {
+					if (sec_item_qty.indexOf(r.message[i].name) === -1) {
+						var row = frappe.model.add_child(frm.doc, frm.fields_dict.sec_item_qty.df.options, frm.fields_dict.sec_item_qty.df.fieldname);
+						row.item_code = r.message[i].name;
+						row.item_code = r.message[i].item_name;
+						
+					}
+				}
+				frm.refresh_field('sec_item_qty');
 				
 			}
 		})
