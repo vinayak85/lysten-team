2e33f109f1df81e1bd3c8afb32af6bc4b28ba93b
vinayak85 <vinupatil9@gmail.com>
2017-11-29 18:57:52 +0530

    Update secondary.js

--- team/team/doctype/secondary/secondary.js
+++ team/team/doctype/secondary/secondary.js
@@ -41,7 +41,8 @@ frappe.ui.form.on('Secondary', {
 			}
 		})
 		
-	},
+	}
+	});
 	
 	test= function(frm,check_item_name) {
 		//alert("hii");
@@ -58,5 +59,5 @@ frappe.ui.form.on('Secondary', {
 		
 		
 		
-	},
-});
+	};
+
