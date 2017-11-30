5df1469d2d82db5570378b226c175f692e79e3a9
vinayak85 <vinupatil9@gmail.com>
2017-11-30 18:28:53 +0530

    Update secondary.py

--- team/team/doctype/secondary/secondary.py
+++ team/team/doctype/secondary/secondary.py
@@ -25,8 +25,68 @@ def get_items(**args):
 	#frappe.msgprint(_("hii"));
 	#frappe.msgprint(_(frappe.get_list('Item',filters=args['filters'], fields=['name', 'item_name'])));
 	#return frappe.get_list('Item',filters=args['filters'], fields=['name', 'item_name'])
-	return frappe.db.sql("""SELECT name,item_name FROM 1bd3e0294da19198.tabItem
-	where 1bd3e0294da19198.tabItem.used_for_secondary=1""", as_dict=1)
+	#return frappe.db.sql("""SELECT name,item_name FROM 1bd3e0294da19198.tabItem
+	#where 1bd3e0294da19198.tabItem.used_for_secondary=1""", as_dict=1)
+	return frappe.db.sql("""SELECT  tbl.item_name as name ,tbl.item_name as item_name,
+sum(tbl.qty) as qty, sum(tbl.f_qty) as f_qty,sum(tbl.tot_qty) as tot_qty ,
+sum(tbl.q_amt) as q_amt,(sum(tbl.f_amt)) as f_amt,
+avg(tbl.avg_rate) as avg_rate,
+sum(tbl.cr_qty) as cr_qty, sum(tbl.cr_f_qty) as cr_f_qty,sum(tbl.cr_tot_qty) as cr_tot_qty,
+sum(tbl.cr_q_amt) as cr_q_amt,(sum(tbl.cr_f_amt)) as cr_f_amt,
+avg(tbl.cr_avg_rate) as cr_avg_rate from
+(select  t_i.name as item_name,
+0 as qty, 0 as f_qty,0 as tot_qty,
+0 as q_amt,0 as f_amt,
+0 as avg_rate,
+0 as cr_qty, 0 as cr_f_qty,0 as cr_tot_qty,
+0 as cr_q_amt,0 as cr_f_amt,
+0 as cr_avg_rate
+from tabItem as t_i
+where t_i.used_for_secondary=1
+group by  t_i.item_name
+
+UNION
+select  t_i.name as item_name,
+sum(t_sit.qty) as qty, sum(t_sit.free_quantity) as f_qty,(sum(t_sit.qty) +sum(t_sit.free_quantity)) as tot_qty,
+sum(t_sit.base_net_amount) as q_amt,(sum(t_sit.free_quantity)*avg(t_sit.base_net_rate)) as f_amt,
+avg(t_sit.base_net_rate) as avg_rate,
+0 as cr_qty, 0 as cr_f_qty,0 as cr_tot_qty,
+0 as cr_q_amt,0 as cr_f_amt,
+avg(t_sit.base_net_rate) as cr_avg_rate
+from tabItem as t_i
+LEFT OUTER JOIN 1bd3e0294da19198.`tabSales Invoice Item` as t_sit
+on t_i.`name`=t_sit.`item_name`
+LEFT OUTER JOIN 1bd3e0294da19198.`tabSales Invoice` as t_si
+on t_si.`name`=t_sit.`parent`
+where t_i.used_for_secondary=1
+and t_si.`posting_date` like '2017-07%'
+and t_si.`customer`='C.T. ENTERPRISES'
+and t_si.`name` like 'SI-%'
+group by  t_i.item_name
+
+union
+
+select  t_i.name as item_name,
+0 as qty, 0 as f_qty,0 as tot_qty,
+0 as q_amt,0 as f_amt,
+0 as avg_rate,
+sum(t_sit.qty) as cr_qty, sum(t_sit.free_quantity) as cr_f_qty,(sum(t_sit.qty) +sum(t_sit.free_quantity)) as cr_tot_qty,
+sum(t_sit.base_net_amount) as cr_q_amt,(sum(t_sit.free_quantity)*avg(t_sit.base_net_rate)) as cr_f_amt,
+avg(t_sit.base_net_rate) as cr_avg_rate
+from tabItem as t_i
+LEFT OUTER JOIN 1bd3e0294da19198.`tabSales Invoice Item` as t_sit
+on t_i.`name`=t_sit.`item_name`
+LEFT OUTER JOIN 1bd3e0294da19198.`tabSales Invoice` as t_si
+on t_si.`name`=t_sit.`parent`
+where t_i.used_for_secondary=1
+and t_si.`posting_date` like '2017-07%'
+and t_si.`customer`='C.T. ENTERPRISES'
+and t_si.`name` like 'SR-0%'
+group by  t_i.item_name) 
+as tbl
+group by   tbl.item_name
+order by tbl.item_name
+""", as_dict=1)
 
 
 class Secondary(Document):
