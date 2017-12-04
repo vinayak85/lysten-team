0b16b5362c00e37c562d6703bc1cdacc2a62b409
vinayak85 <vinupatil9@gmail.com>
2017-12-04 18:45:58 +0530

    Update attendence_calculation.py

--- team/team/doctype/attendence_calculation/attendence_calculation.py
+++ team/team/doctype/attendence_calculation/attendence_calculation.py
@@ -5,6 +5,16 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
+from frappe import msgprint, _ 
+import frappe.utils
+from frappe.utils import cstr, flt, getdate, cint
+from frappe.model.naming import make_autoname
+from frappe import _
+from frappe.model.mapper import get_mapped_doc
+
+@frappe.whitelist()
+def get_calculations(fromdate, todate):
+	return frappe.db.sql("""call attendence1({0},{1})""".format(fromdate,todate), as_dict=1);
 
 class AttendenceCalculation(Document):
 	pass
