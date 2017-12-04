9fb3c15960df1d208f819740b87311c0ee96c30c
vinayak85 <vinupatil9@gmail.com>
2017-12-04 18:56:17 +0530

    Update attendence_calculation.js

--- team/team/doctype/attendence_calculation/attendence_calculation.js
+++ team/team/doctype/attendence_calculation/attendence_calculation.js
@@ -1,8 +1,57 @@
 // Copyright (c) 2017, Vinayak Patil and contributors
 // For license information, please see license.txt
+//get_calculations
 
 frappe.ui.form.on('Attendence Calculation', {
 	refresh: function(frm) {
-
+		frm.add_custom_button(__("Recalculate Attendence"),
+			function() { frm.trigger('get_items_all'); }, "fa fa-sitemap", "btn-default");
+	},
+	get_items_all:function (frm) {
+		
+		frappe.call({
+			method:'team.team.doctype.attendence_calculation.attendence_calculation.get_calculations',
+			args:{
+				fromdate: '2017/11/01',
+				todate: '2017/11/01',
+				
+			},
+			callback:function (r) {
+				//alert(r.message[1].name);
+				var attendence_table = $.map(frm.doc.attendence_table, function(d) { return d.sec_item_qty });
+				//alert(sec_items_qty );
+				for (var i=0; i< r.message.length; i++) {
+					
+					if (sec_items_qty.indexOf(r.message[i].att_date) === -1) {
+					      
+						
+						row.att_date = r.message[i].att_date;
+						row.emp_code = r.message[i].emp_code;
+						row.email = r.message[i].email;
+						
+						row.app_presenty = r.message[i].app_presenty;
+						row.lysten_presenty = r.message[i].lysten_presenty;
+						row.objective = r.message[i].objective;
+					
+						row.working_flag = r.message[i].working_flag;
+						row.dcr = r.message[i].dcr;
+						row.chem_call = r.message[i].chem_call;
+						row.meeting = r.message[i].meeting;
+						row.camp = r.message[i].camp;
+						row.lve = r.message[i].lve;
+						
+						
+				
+					}					
+					
+				}
+				frm.refresh_field('attendence_table');
+				
+				
+			}
+		})
+		
 	}
-});
+	});
+	
+
