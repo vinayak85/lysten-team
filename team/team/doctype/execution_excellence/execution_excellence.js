8fdaf26c4778b8c5bcfde825b02983f42dd24f65
vinayak85 <vinupatil9@gmail.com>
2017-12-05 17:13:02 +0530

    Update execution_excellence.js

--- team/team/doctype/execution_excellence/execution_excellence.js
+++ team/team/doctype/execution_excellence/execution_excellence.js
@@ -19,9 +19,17 @@ remove_child_rows= function(frm) {
 	var tbl = frm.doc.execution_excellence_table || [];
 	var i = tbl.length;
 	while (i--){ 
-	//for (var i=0; i< 12; i++) {
-	alert(	get_month(0));
-	//}
+	for (var i=0; i< 12; i++) {
+		var row = frappe.model.add_child(frm.doc, frm.fields_dict.execution_excellence_table.df.options,
+						 frm.fields_dict.execution_excellence_table.df.fieldname);
+		
+		row.month = get_month(0);
+		row.total_days_month = "30";
+		row.holidays = "5";
+		row.working = "25";
+
+		//alert(	get_month(0));
+	}
         frm.get_field("execution_excellence_table").grid.grid_rows[i].remove();
 	}
 	 frm.refresh_field('execution_excellence_table');
@@ -31,6 +39,46 @@ get_month=function(i)
 	if(i==0)
 	{
 		return "April";
+	}else if(i==1)
+	{
+		return "May";
+	}else if(i==2)
+	{
+		return "June";
+	}else if(i==3)
+	{
+		return "July";
+	}
+	else if(i==4)
+	{
+		return "Aug";
+	}
+	else if(i==5)
+	{
+		return "Sept";
+	}
+	else if(i==6)
+	{
+		return "Oct";
+	}
+	else if(i==7)
+	{
+		return "Nov";
+	}
+	else if(i==8)
+	{
+		return "Dec";
+	}
+	else if(i==9)
+	{
+		return "Jan";
+	}
+	else if(i==10)
+	{
+		return "Feb";
+	}else if(i==11)
+	{
+		return "Mar";
 	}
 };
 
