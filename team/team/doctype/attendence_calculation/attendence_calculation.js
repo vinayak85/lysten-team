ee90e32fa763e801096a7110646ce087cfb64bd7
vinayak85 <vinupatil9@gmail.com>
2017-12-05 18:00:36 +0530

    Update attendence_calculation.js

--- team/team/doctype/attendence_calculation/attendence_calculation.js
+++ team/team/doctype/attendence_calculation/attendence_calculation.js
@@ -17,14 +17,14 @@ frappe.ui.form.on('Attendence Calculation', {
 				
 			},
 			callback:function (r) {
-				//alert(r.message);
+				alert("1");
 				var attendence_tables = $.map(frm.doc.attendence_table, function(d) { return d.attendence_table });
-				//alert(sec_items_qty );
+				alert("2");
 				for (var i=0; i< r.message.length; i++) {
-					
+					alert("3");
 					if (attendence_tables.indexOf(r.message[i].att_date) === -1) {
 					      
-						
+						alert("4");
 						row.att_date = r.message[i].att_date;
 						row.emp_code = r.message[i].emp_code;
 						row.email = r.message[i].email;
