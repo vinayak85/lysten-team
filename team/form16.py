c62669c85e181aef92aa0931833c811fb9e40f4c
vinayak85 <vinupatil9@gmail.com>
2017-12-12 16:52:00 +0530

    Update form16.py

--- team/form16.py
+++ team/form16.py
@@ -6,11 +6,18 @@ from pytz import timezone
 from frappe import msgprint, _
 __version__ = '0.0.1'
 
-@frappe.whitelist()
-def form16_package():
-    package='13.127.22.180';
+@frappe.whitelist(employee)
+def form16_package():    
+    package_cnt='';
+    if(len(employee)>0):
+        employee="'"+employee+"'";
+        package_cnt= frappe.db.sql("""select  count(distinct base)as cnt from `tabSalary Structure Employee` where employee={0} 
+        """.format(employee), as_dict=1)
+    if(package_cnt[0].cnt>1):
+        package_cnt= frappe.db.sql("""select base from `tabSalary Structure Employee` where employee={0} 
+        """.format(employee), as_dict=1)        
     dict = {'package': ''
            }
     
-    dict['package']=ip;
+    dict['package']=package;
     return dict
