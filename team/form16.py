a25ef95447cc44aa403dd328599a4f207d9bbe65
vinayak85 <vinupatil9@gmail.com>
2017-12-12 15:29:40 +0530

    Update form16.py

--- team/form16.py
+++ team/form16.py
@@ -1,1 +1,16 @@
+from __future__ import unicode_literals
+import frappe
+import datetime
+from datetime import datetime
+from pytz import timezone
+from frappe import msgprint, _
+__version__ = '0.0.1'
 
+@frappe.whitelist()
+def form16_package():
+    package='13.127.22.180';
+    dict = {'package': ''
+           }
+    
+    dict['package']=ip;
+    return dict
