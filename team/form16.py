33c39951ac58b7b77f7c612f35dca59ab02163dd
vinayak85 <vinupatil9@gmail.com>
2017-12-12 16:54:59 +0530

    Update form16.py

--- team/form16.py
+++ team/form16.py
@@ -9,15 +9,24 @@ __version__ = '0.0.1'
 @frappe.whitelist(employee)
 def form16_package():    
     package_cnt='';
+    package='';
+    
     if(len(employee)>0):
         employee="'"+employee+"'";
         package_cnt= frappe.db.sql("""select  count(distinct base)as cnt from `tabSalary Structure Employee` where employee={0} 
         """.format(employee), as_dict=1)
+        
     if(package_cnt[0].cnt>1):
-        package_cnt= frappe.db.sql("""select base from `tabSalary Structure Employee` where employee={0} 
-        """.format(employee), as_dict=1)        
+        employee="'"+employee+"'";
+        package= frappe.db.sql("""select GROUP_CONCAT(distinct base) as pckge from `tabSalary Structure Employee` where employee={0} 
+        """.format(employee), as_dict=1) 
+    else:
+        employee="'"+employee+"'";
+        package= frappe.db.sql("""select base as pckge from `tabSalary Structure Employee` where employee={0} 
+        """.format(employee), as_dict=1)
+        
     dict = {'package': ''
            }
     
-    dict['package']=package;
+    dict['package']=package[0].pckge;
     return dict
