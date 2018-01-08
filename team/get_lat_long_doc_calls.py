e230c4438426cff9f1b3fff1fd4046069564a5a1
vinayak85 <vinupatil9@gmail.com>
2018-01-08 17:34:34 +0530

    Update get_lat_long_doc_calls.py

--- team/get_lat_long_doc_calls.py
+++ team/get_lat_long_doc_calls.py
@@ -13,8 +13,7 @@ from frappe.model.mapper import get_mapped_doc
 @frappe.whitelist()
 def get_lat_long_details(emp, date):
 	#doc_name=year + "-" + month + "-" + stockist;
-	c = frappe.db.sql("""SELECT name,latitude,longitude,SUBSTRING(creation, 12, 5) as time_call,doctor_name,patch_id,jwf_with,jwf_with2 
-  FROM 1bd3e0294da19198.`tabDoctor Calls`where date={0} and dr_call_by_user_id={1} """.
-  format("'"+date+"'","'"+emp+"'"), as_dict=1);
+	c = frappe.db.sql("""SELECT name,latitude,longitude,SUBSTRING(creation, 12, 5) as time_call,doctor_name,patch_id,jwf_with,jwf_with2,
+	comment FROM 1bd3e0294da19198.`tabDoctor Calls`where date={0} and dr_call_by_user_id={1} """.format("'"+date+"'","'"+emp+"'"), as_dict=1);
 	
 	return c;
