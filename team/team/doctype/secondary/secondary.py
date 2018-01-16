218b3115e88027849a2066ad292fd21787d712d5
vinayak85 <vinupatil9@gmail.com>
2018-01-16 16:11:32 +0530

    Update secondary.py

--- team/team/doctype/secondary/secondary.py
+++ team/team/doctype/secondary/secondary.py
@@ -230,8 +230,8 @@ class Secondary(Document):
 	if(duplicate_pos>=0):
 		frappe.throw(_("Duplicate Item "+self.sec_items_qty[duplicate_pos].item_code2))
 	else:
-		var aa=', '.join("%s: %s" % item for item in self.__dict__.keys());
-		frappe.msgprint(_(aa));
+		
+		frappe.msgprint(_(self.__dict__.keys()));
 		new_name = self.year + "-" + self.month + "-" + self.stockist;
 		if self.name != new_name and not self.is_new():
 			frappe.rename_doc(self.doctype,self.name,new_name)
