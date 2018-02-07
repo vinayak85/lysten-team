d1e5bff4926bea6a1aba749f257018a930feb451
vinayak85 <vinupatil9@gmail.com>
2018-02-07 23:52:50 +0530

    Update chart_secondary.py

--- team/chart_secondary.py
+++ team/chart_secondary.py
@@ -10,10 +10,10 @@ __version__ = '0.0.1'
 def get_date_and_app_support():
     app_ver_count = frappe.db.sql("""select sum(opn_tot*item_rate) as "Opening",sum(rec_tot*item_rate) as "Primary/Received",
     sum(close_tot*item_rate) as "Closing",sum(sale_tot*item_rate) as "Secondary/Sale" from `tabsec_item_qty` where parent  
-    like '2017-Oct-CHIRAYU PHARMA'""", as_dict=1)
+    like '2017-Oct-CHIRAYU PHARMA'""", as_dict=0)
 
-    dict = {'app_ver_count': 0
-            }
+    #dict = {'app_ver_count': 0}
    
-    dict['app_ver_count'] = app_ver_count;
-    return dict
+    #dict['app_ver_count'] = app_ver_count;
+    #return dict
+    return app_ver_count;
