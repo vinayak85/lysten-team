00688eb7ae6a58316814c94630c2b5dfad546821
vinayak85 <vinupatil9@gmail.com>
2018-02-08 00:10:57 +0530

    Update chart_secondary.py

--- team/chart_secondary.py
+++ team/chart_secondary.py
@@ -8,12 +8,20 @@ __version__ = '0.0.1'
 
 @frappe.whitelist()
 def get_date_and_app_support():
-    app_ver_count = frappe.db.sql("""select sum(opn_tot*item_rate) as "Opening",sum(rec_tot*item_rate) as "Primary/Received",
+    
+    july = frappe.db.sql("""select sum(opn_tot*item_rate) as "Opening",sum(rec_tot*item_rate) as "Primary/Received",
     sum(close_tot*item_rate) as "Closing",sum(sale_tot*item_rate) as "Secondary/Sale" from `tabsec_item_qty` where parent  
-    like '2017-Oct-CHIRAYU PHARMA'""", as_dict=1)
+    like '2017-Oct-CHIRAYU PHARMA'""", as_dict=0)
+    
+    aug = frappe.db.sql("""select sum(opn_tot*item_rate) as "Opening",sum(rec_tot*item_rate) as "Primary/Received",
+    sum(close_tot*item_rate) as "Closing",sum(sale_tot*item_rate) as "Secondary/Sale" from `tabsec_item_qty` where parent  
+    like '2017-Oct-CHIRAYU PHARMA'""", as_dict=0)
+    datasets = []
+    datasets.append({'title': 'July', 'values': july})
+	datasets.append({'title': 'Aug', 'values': aug})
 
-    #dict = {'app_ver_count': 0}
+    dict = {'app_ver_count': 0}
    
-    #dict['app_ver_count'] = app_ver_count;
-    #return dict
-    return app_ver_count;
+    dict['app_ver_count'] = datasets;
+    return dict
+    #return datasets;
