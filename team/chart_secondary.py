722541b9ead745b89e20fe199297486867a85a28
vinayak85 <vinupatil9@gmail.com>
2018-02-08 14:23:59 +0530

    Update chart_secondary.py

--- team/chart_secondary.py
+++ team/chart_secondary.py
@@ -9,32 +9,39 @@ __version__ = '0.0.1'
 @frappe.whitelist()
 def get_date_and_app_support():
     
-    july = frappe.db.sql("""select sum(opn_tot*item_rate) as "Opening",sum(rec_tot*item_rate) as "Primary/Received",
-    sum(close_tot*item_rate) as "Closing",sum(sale_tot*item_rate) as "Secondary/Sale" from `tabsec_item_qty` where parent  
+    july = frappe.db.sql("""select sum(opn_qty*item_rate) as "Opening",sum(rec_qty*item_rate) as "Primary/Received",
+    sum(close_qty*item_rate) as "Closing",sum(value_credit_note_qty) as "Credit note"
+    ,sum(sale_qty*item_rate) as "Secondary/Sale" from `tabsec_item_qty` where parent  
     like '2017-July-CHIRAYU PHARMA'""", as_dict=0)
     
-    aug = frappe.db.sql("""select sum(opn_tot*item_rate) as "Opening",sum(rec_tot*item_rate) as "Primary/Received",
-    sum(close_tot*item_rate) as "Closing",sum(sale_tot*item_rate) as "Secondary/Sale" from `tabsec_item_qty` where parent  
+    aug = frappe.db.sql("""select sum(opn_qty*item_rate) as "Opening",sum(rec_qty*item_rate) as "Primary/Received",
+    sum(close_qty*item_rate) as "Closing",sum(value_credit_note_qty) as "Credit note",
+    sum(sale_qty*item_rate) as "Secondary/Sale" from `tabsec_item_qty` where parent  
     like '2017-Aug-CHIRAYU PHARMA'""", as_dict=0)
     
-    sept = frappe.db.sql("""select sum(opn_tot*item_rate) as "Opening",sum(rec_tot*item_rate) as "Primary/Received",
-    sum(close_tot*item_rate) as "Closing",sum(sale_tot*item_rate) as "Secondary/Sale" from `tabsec_item_qty` where parent  
+    sept = frappe.db.sql("""select sum(opn_qty*item_rate) as "Opening",sum(rec_qty*item_rate) as "Primary/Received",
+    sum(close_qty*item_rate) as "Closing",sum(value_credit_note_qty) as "Credit note",
+    sum(sale_qty*item_rate) as "Secondary/Sale" from `tabsec_item_qty` where parent  
     like '2017-Sept-CHIRAYU PHARMA'""", as_dict=0)
     
-    oct = frappe.db.sql("""select sum(opn_tot*item_rate) as "Opening",sum(rec_tot*item_rate) as "Primary/Received",
-    sum(close_tot*item_rate) as "Closing",sum(sale_tot*item_rate) as "Secondary/Sale" from `tabsec_item_qty` where parent  
+    oct = frappe.db.sql("""select sum(opn_qty*item_rate) as "Opening",sum(rec_qty*item_rate) as "Primary/Received",
+    sum(close_qty*item_rate) as "Closing",sum(value_credit_note_qty) as "Credit note",
+    sum(sale_qty*item_rate) as "Secondary/Sale" from `tabsec_item_qty` where parent  
     like '2017-Oct-CHIRAYU PHARMA'""", as_dict=0)
     
-    nov = frappe.db.sql("""select sum(opn_tot*item_rate) as "Opening",sum(rec_tot*item_rate) as "Primary/Received",
-    sum(close_tot*item_rate) as "Closing",sum(sale_tot*item_rate) as "Secondary/Sale" from `tabsec_item_qty` where parent  
+    nov = frappe.db.sql("""select sum(opn_qty*item_rate) as "Opening",sum(rec_qty*item_rate) as "Primary/Received",
+    sum(close_qty*item_rate) as "Closing",sum(value_credit_note_qty) as "Credit note",
+    sum(sale_qty*item_rate) as "Secondary/Sale" from `tabsec_item_qty` where parent  
     like '2017-Nov-CHIRAYU PHARMA'""", as_dict=0)
     
-    dec = frappe.db.sql("""select sum(opn_tot*item_rate) as "Opening",sum(rec_tot*item_rate) as "Primary/Received",
-    sum(close_tot*item_rate) as "Closing",sum(sale_tot*item_rate) as "Secondary/Sale" from `tabsec_item_qty` where parent  
+    dec = frappe.db.sql("""select sum(opn_qty*item_rate) as "Opening",sum(rec_qty*item_rate) as "Primary/Received",
+    sum(close_qty*item_rate) as "Closing",sum(value_credit_note_qty) as "Credit note",
+    sum(sale_qty*item_rate) as "Secondary/Sale" from `tabsec_item_qty` where parent  
     like '2017-Dec-CHIRAYU PHARMA'""", as_dict=0)
     
-    jan = frappe.db.sql("""select sum(opn_tot*item_rate) as "Opening",sum(rec_tot*item_rate) as "Primary/Received",
-    sum(close_tot*item_rate) as "Closing",sum(sale_tot*item_rate) as "Secondary/Sale" from `tabsec_item_qty` where parent  
+    jan = frappe.db.sql("""select sum(opn_qty*item_rate) as "Opening",sum(rec_qty*item_rate) as "Primary/Received",
+    sum(close_qty*item_rate) as "Closing",sum(value_credit_note_qty) as "Credit note",
+    sum(sale_qty*item_rate) as "Secondary/Sale" from `tabsec_item_qty` where parent  
     like '2018-Jan-CHIRAYU PHARMA'""", as_dict=0)
     
    
