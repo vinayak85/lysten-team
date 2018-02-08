7cf899bb512cb32b60458600e6c75f8b17192f38
vinayak85 <vinupatil9@gmail.com>
2018-02-08 19:14:47 +0530

    Update chart_secondary.py

--- team/chart_secondary.py
+++ team/chart_secondary.py
@@ -76,19 +76,19 @@ def getmonthly(months):
         opening.append(opn[0].opn);
         
         prim=frappe.db.sql("""select sum(rec_qty*item_rate) as prim from `tabsec_item_qty` where parent  
-    like '2017-Aug-CHIRAYU PHARMA'""", as_dict=1);
+    like concat({0},'-CHIRAYU PHARMA')""".format("'"+f+"'"), as_dict=1);
         primary.append(prim[0].prim);
             
         clos=frappe.db.sql("""select sum(close_qty*item_rate) as clos from `tabsec_item_qty` where parent  
-    like '2017-Sept-CHIRAYU PHARMA'""", as_dict=1);
+    like concat({0},'-CHIRAYU PHARMA')""".format("'"+f+"'"), as_dict=1);
         closing.append(clos[0].clos);
         
         cred=frappe.db.sql("""select sum(value_credit_note_qty) as cred from `tabsec_item_qty` where parent  
-    like '2017-Oct-CHIRAYU PHARMA'""", as_dict=1);
+    like concat({0},'-CHIRAYU PHARMA')""".format("'"+f+"'"), as_dict=1);
         credit.append(cred[0].cred);
         
         sale=frappe.db.sql("""select sum(sale_qty*item_rate) as sale from `tabsec_item_qty` where parent  
-    like '2017-Nov-CHIRAYU PHARMA'""", as_dict=1);
+    like concat({0},'-CHIRAYU PHARMA')""".format("'"+f+"'"), as_dict=1);
         saling.append(sale[0].sale);
         pass;
     datasets = [];
