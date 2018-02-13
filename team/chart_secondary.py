a168124a22ad3a252662913b62bac400ce3b2e4e
vinayak85 <vinupatil9@gmail.com>
2018-02-13 14:46:50 +0530

    Update chart_secondary.py

--- team/chart_secondary.py
+++ team/chart_secondary.py
@@ -116,8 +116,8 @@ def getproductwise(stockist_name,products,months):
         
         frappe.msgprint(_("sec Id : "+ss+" prod"+f));
         
-        sales = frappe.db.sql("""select sum(sale_qty*item_rate) as sale from `tabsec_item_qty` where parent 
-        in({0}) and  item_code2={1}""".format(ss,f), as_dict=1)
+        ##sales = frappe.db.sql("""select sum(sale_qty*item_rate) as sale from `tabsec_item_qty` where parent 
+        ##in({0}) and  item_code2={1}""".format(ss,f), as_dict=1)
         
         #sal=sales[0].sale;
         ##frappe.msgprint(_(sales[0].sale));
