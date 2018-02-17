4ec2ab5d449f478f39613d73d42d4980d72cad59
vinayak85 <vinupatil9@gmail.com>
2018-02-17 13:41:42 +0530

    Update chart_report_employee_calls.py

--- team/chart_report_employee_calls.py
+++ team/chart_report_employee_calls.py
@@ -24,8 +24,8 @@ def get_call_summary():
   c = time.strptime('2018-01-09 08:00:00',"%Y-%m-%d %H:%M:%S")
   t = time.mktime(c) 
   t = t + 1800 #30 minutes is 1800 secs
-  dt=time.mktime(t)
-  frappe.msgprint(_("time:"+": "+str(dt)));
+  #dt=time.mktime(t)
+  frappe.msgprint(_("time:"+": "+str(c)));
  
 def local_time(zone='Asia/Kolkata'):
     other_zone = timezone(zone)
