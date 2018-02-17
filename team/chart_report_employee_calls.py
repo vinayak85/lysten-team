f8593bb9bb65e81973fc1e17e85c174d9aa8d6ce
vinayak85 <vinupatil9@gmail.com>
2018-02-17 11:39:17 +0530

    Update chart_report_employee_calls.py

--- team/chart_report_employee_calls.py
+++ team/chart_report_employee_calls.py
@@ -1,1 +1,13 @@
+from __future__ import unicode_literals
+import frappe
+from datetime import datetime
+from datetime import datetime, timedelta
+from frappe import msgprint, _
 
+__version__ = '0.0.1'
+
+
+@frappe.whitelist()
+def get_call_summary():  
+  nine_hours_from_now = datetime.now() + timedelta(hours=9)
+  frappe.msgprint(_("time:"+": "+datetime.now()+"   "+nine_hours_from_now));
