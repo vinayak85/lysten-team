308db676abab2be92fae2c612d83a2cdac23bc02
vinayak85 <vinupatil9@gmail.com>
2018-02-17 12:18:38 +0530

    Update chart_report_employee_calls.py

--- team/chart_report_employee_calls.py
+++ team/chart_report_employee_calls.py
@@ -1,9 +1,9 @@
 from __future__ import unicode_literals
 import frappe
+import datetime
 from datetime import datetime
-import time
+from pytz import timezone
 from frappe import msgprint, _
-
 __version__ = '0.0.1'
 
 
@@ -13,3 +13,8 @@ def get_call_summary():
   #nine_hours_from_now = datetime.now() + timedelta(hours=9)
   current_time = local_time()
   frappe.msgprint(_("time:"+": "+current_time));
+ 
+def local_time(zone='Asia/Kolkata'):
+    other_zone = timezone(zone)
+    other_zone_time = datetime.now(other_zone)
+    return other_zone_time.strftime('%T')
