943844c5164a1cbca48e0b50f5c91e8576f4b389
vinayak85 <vinupatil9@gmail.com>
2018-02-17 12:48:58 +0530

    Update chart_report_employee_calls.py

--- team/chart_report_employee_calls.py
+++ team/chart_report_employee_calls.py
@@ -13,7 +13,7 @@ def get_call_summary():
   #from datetime import datetime, timedelta
   #nine_hours_from_now = datetime.now() + timedelta(hours=9)
   #current_time = local_time()
-  now = frappe.utils.now()
+  now = frappe.utils.now().split(' ')[1]
   #now_plus_10 = (datetime.datetime.now() + datetime.timedelta(minutes=15)).strftime("%H:%M:%S")
   frappe.msgprint(_("time:"+": "+str(now)));
  
