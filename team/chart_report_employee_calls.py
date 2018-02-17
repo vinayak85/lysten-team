ab077cbe5d3324a7285e120ebc3e332db4c10817
vinayak85 <vinupatil9@gmail.com>
2018-02-17 13:52:48 +0530

    Update chart_report_employee_calls.py

--- team/chart_report_employee_calls.py
+++ team/chart_report_employee_calls.py
@@ -10,23 +10,14 @@ __version__ = '0.0.1'
 
 
 @frappe.whitelist()
-def get_call_summary():  
-  #from datetime import datetime, timedelta
-  #nine_hours_from_now = datetime.now() + timedelta(hours=9)
-  #current_time = local_time()
+def get_call_summary(): 
+  dt='2018-01-09 08:00:00'
+  c = time.strptime(dt,"%Y-%m-%d %H:%M:%S")
   
-  #now = frappe.utils.now().split(' ')[1]
-  
-  #now = frappe.utils.data.nowtime ()
-  #now_plus_10 =  frappe.utils.data.to_timedelta(10)
-  #frappe.msgprint(_("time:"+": "+str(now)+"  "+str(now_plus_10)));
-  
-  c = time.strptime('2018-01-09 08:00:00',"%Y-%m-%d %H:%M:%S")
   t = time.mktime(c) 
   t = t + 1800 #30 minutes is 1800 secs
-  #dt=time.mktime(t)
-  #dt=time.strftime("%Y-%m-%d %H:%M:%S", t)
   dt_obj = datetime.fromtimestamp(t)
+  
   frappe.msgprint(_("time:"+": "+str(dt_obj)));
  
 def local_time(zone='Asia/Kolkata'):
