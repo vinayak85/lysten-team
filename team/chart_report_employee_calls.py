8d8b19c671c5ee76d7725949e02db2bad5d7a631
vinayak85 <vinupatil9@gmail.com>
2018-02-17 17:56:50 +0530

    Update chart_report_employee_calls.py

--- team/chart_report_employee_calls.py
+++ team/chart_report_employee_calls.py
@@ -18,37 +18,46 @@ def get_call_summary():
   intervaltime=60*60;
   workstarttime='08:00:00';
   workendtime='21:00:00';
-  #while (fromdate <= todate):
+  list_of_cnt=[];
+  while (fromdate <= todate):
     #frappe.msgprint(_("time:"+": "+str(fromdate)));
-    #fromdate=frappe.utils.data.add_days (fromdate, 1);
-    #frappe.msgprint(_("time:"+": "+str(fromdate)));
-  #dt_form_s_time=fromdate+' '+workstarttime
-  cntcall=[];
-  flag=0;
-  while (workstarttime < workendtime):
-    dt_form_s_time=fromdate+' '+workstarttime
-    c = time.strptime(dt_form_s_time,"%Y-%m-%d %H:%M:%S") 
-    t = time.mktime(c) 
-    t = t + intervaltime #30 minutes is 1800 secs
-    dt_form_t_time = datetime.fromtimestamp(t)  
-    #frappe.msgprint(_("time:"+": "+str(dt_form_t_time)));
-    workstarttime = str(dt_form_t_time).split(' ')[1]
-    curr_select_date = str(dt_form_t_time).split(' ')[0]
-    #frappe.msgprint(_("time:"+": "+str(workstarttime))); 
+    cntcall=[];
+    flag=0;
     
-    #frappe.msgprint(_("From:"+": "+str(dt_form_s_time)+" To:"+str(dt_form_t_time)));
+    while (workstarttime < workendtime):
+      
+      dt_form_s_time=fromdate+' '+workstarttime
+      c = time.strptime(dt_form_s_time,"%Y-%m-%d %H:%M:%S") 
+      t = time.mktime(c) 
+      t = t + intervaltime #30 minutes is 1800 secs
+      dt_form_t_time = datetime.fromtimestamp(t)  #calculate to_time using interval time .this may be 30min,1hr,2hr
+      
+      #frappe.msgprint(_("time:"+": "+str(dt_form_t_time)));
+      
+      workstarttime = str(dt_form_t_time).split(' ')[1] #get time and use swap method like c=a,a=b,b=c
+      curr_select_date = str(dt_form_t_time).split(' ')[0]#get task perform curr date for add in list at position 0
+      
+      #frappe.msgprint(_("time:"+": "+str(workstarttime)));#frappe.msgprint(_("From:"+": "+str(dt_form_s_time)+" To:"+str(dt_form_t_time)));
+      
+      cnt_dcr = frappe.db.sql(""" select count(*) as cnt from  1bd3e0294da19198.`tabDoctor Calls` 
+      where creation between {0} and {1}; """.format("'"+str(dt_form_s_time)+"'","'"+str(dt_form_t_time)+"'"), as_dict=1)
+      
+      #frappe.msgprint(_(cnt_dcr[0].cnt));
+      if(flag==0):
+        cntcall.insert(0,curr_select_date);
+      
+      cntcall.append(cnt_dcr[0].cnt);
+      flag=1;
+      #for p in cntcall: 
+        #frappe.msgprint(_(p));
+        
+    list_of_cnt.append(cntcall);
+    fromdate=frappe.utils.data.add_days (fromdate, 1); #use for while loop increment in from_date by 1 day upto to_date
+    #frappe.msgprint(_("time:"+": "+str(fromdate))); 
+    for p in list_of_cnt: 
+      frappe.msgprint(_(p));
     
-    cnt_dcr = frappe.db.sql(""" select count(*) as cnt from  1bd3e0294da19198.`tabDoctor Calls` 
-    where creation between {0} and {1}; """.format("'"+str(dt_form_s_time)+"'","'"+str(dt_form_t_time)+"'"), as_dict=1)
-              
-    #frappe.msgprint(_(cnt_dcr[0].cnt));
-    if(flag==0):
-      cntcall.insert(0,curr_select_date);
-    cntcall.append(cnt_dcr[0].cnt);  
-    flag=1;
     
-  for p in cntcall: 
-    frappe.msgprint(_(p));
   #fromdate=frappe.utils.data.add_days (fromdate, 2);
   #if(fromdate <= todate):
     #frappe.msgprint(_("less than from date:"));
