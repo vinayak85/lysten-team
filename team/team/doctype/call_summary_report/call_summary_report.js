a937a5cf4103e4e27618aa0227acf65fab84ae5b
vinayak85 <vinupatil9@gmail.com>
2018-02-21 07:02:54 +0000

    comment

---
+++ team/team/doctype/call_summary_report/call_summary_report.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2018, Vinayak Patil and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Call Summary Report', {
+	refresh: function(frm) {
+
+	}
+});
