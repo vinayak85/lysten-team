205b9cf3bde6be0890cb9c11ff99fecc550ea765
vinayak85 <vinupatil9@gmail.com>
2018-03-09 17:33:10 +0530

    Update user_lock.py

--- team/user_lock.py
+++ team/user_lock.py
@@ -117,7 +117,7 @@ def update_user_lock_time_and_date(m_pro,m_pat,m_doc,m_che,t_obj_time,t_drc_s_ti
     flag=0;
     frappe.msgprint(_("a"));
     if(m_pro!="" and m_pat!="" and m_doc!="" and m_che!="" and t_obj_time!="" and t_drc_s_time!="" and t_chc_s_time!=""):
-        frappe.msgprint(_(t_obj_time+" "+t_drc_s_time+" "+t_chc_s_time));#m_pro+" "+m_pat+" "+m_doc+" "+m_che+" "+
+        frappe.msgprint(_(m_pro));#m_pro+" "+m_pat+" "+m_doc+" "+m_che+" "+
         frappe.db.sql("""update `tabUser` set m_pro={0},m_pat={1},m_doc={2},m_che={3},
         t_obj_time={4},t_drc_s_time={5},t_chc_s_time={6},t_drc1="",t_drc2="",t_obj1="",
         t_obj2="",t_chc1="",t_chc2="" where enabled=1 and 
