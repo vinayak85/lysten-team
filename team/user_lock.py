bac4c4947a97706b894ca37e02ef579df6153d42
vinayak85 <vinupatil9@gmail.com>
2018-03-13 15:53:46 +0530

    Update user_lock.py

--- team/user_lock.py
+++ team/user_lock.py
@@ -135,9 +135,9 @@ def update_user_lock_time_and_date(m_pro,m_pat,m_doc,m_che,t_obj_time,t_drc_s_ti
     return dict
     
 @frappe.whitelist()
-def retrun_user_list_with_lock_flag():
-	frappe.msgprint(_("c"));
-	return frappe.db.sql("""Select name,concat(first_name," ",last_name) as full_name,
+def retrun_user_list_with_lock_flag(limit, offset):
+   frappe.msgprint(_("c"));
+	'''return frappe.db.sql("""Select name,concat(first_name," ",last_name) as full_name,
 	designation,enabled,modified,ifnull(mobile_no1,"-"),
 	if(m_pro=(select profile_master from `tabStandard Lock` order by modified desc limit 1)
 	&& m_pat=(select patch_master from `tabStandard Lock` order by modified desc limit 1)
@@ -146,7 +146,7 @@ def retrun_user_list_with_lock_flag():
 	if(t_obj_time=(select objective_lock_time from `tabStandard Lock` order by modified desc limit 1)
 	&& t_drc_s_time= (select doctor_start_time from `tabStandard Lock` order by modified desc limit 1)
 	&& t_chc_s_time=(select chemist_start_time from `tabStandard Lock` order by modified desc limit 1),1,0) as trans_flag 
-	from `tabUser` where designation in('TBM','ABM','RBM','ZBM','SM','NBM','CRM');""");
+	from `tabUser` where designation in('TBM','ABM','RBM','ZBM','SM','NBM','CRM');""");'''
 
 
 '''select profile_master,chemist_master,doctor_master,patch_master,chemist_start_time,
