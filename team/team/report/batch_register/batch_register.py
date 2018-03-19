67ddeee6edb62da9e17c4a0d00af95d0c3c92d56
vinayak85 <vinupatil9@gmail.com>
2018-03-19 15:55:53 +0530

    Update batch_register.py

--- team/team/report/batch_register/batch_register.py
+++ team/team/report/batch_register/batch_register.py
@@ -11,32 +11,22 @@ from frappe.utils.xlsxutils import handle_html
 def execute(filters=None):
 	return _execute(filters)
 
-def _execute(filters=None, additional_table_columns=None, additional_query_columns=None):
+def _execute(filters=None,  additional_query_columns=None):
 	columns, data = [], []
 	if not filters: filters = {}
-	columns = get_columns(additional_table_columns)	
+	columns = get_columns()	
 	return columns, data
 
-def get_columns(additional_table_columns):
+def get_columns():
+	
 	columns = [
-		_("Item Code") + ":Link/Item:120", _("Item Name") + "::120",
-		_("Item Group") + ":Link/Item Group:100", _("Invoice") + ":Link/Sales Invoice:120",
-		_("Posting Date") + ":Date:80", _("Customer") + ":Link/Customer:120",
-		_("Customer Name") + "::120"]
-
-	if additional_table_columns:
-		columns += additional_table_columns
-
-	columns += [
-		_("Customer Group") + ":Link/Customer Group:120",
-		_("Receivable Account") + ":Link/Account:120",
-		_("Mode of Payment") + "::120", _("Territory") + ":Link/Territory:80",
-		_("Project") + ":Link/Project:80", _("Company") + ":Link/Company:100",
-		_("Sales Order") + ":Link/Sales Order:100", _("Delivery Note") + ":Link/Delivery Note:100",
-		_("Income Account") + ":Link/Account:140", _("Cost Center") + ":Link/Cost Center:140",
-		_("Stock Qty") + ":Float:120", _("Stock UOM") + "::100",
-		_("Rate") + ":Currency/currency:120",
-		_("Amount") + ":Currency/currency:120"
+		_("Month") + "::120",
+		_("Purchase") + "::120",
+		_("Sale") + "::120",
+		_("Credit Note") + "::120",
+		_("Sample") + "::120",
+		_("Balance") + "::120"
+		
 	]
 
 	return columns
