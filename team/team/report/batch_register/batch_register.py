78acd25ac5d83a90cc3c4e15e483b30260e4690f
vinayak85 <vinupatil9@gmail.com>
2018-03-19 16:05:06 +0530

    Update batch_register.py

--- team/team/report/batch_register/batch_register.py
+++ team/team/report/batch_register/batch_register.py
@@ -28,7 +28,7 @@ def get_columns():
 		_("Sale") + "::120",
 		_("Credit Note") + "::120",
 		_("Sample") + "::120",
-		_("Balance") + "::120"
+		_("Balance") + "::120",
 		_("Batch No") + "::120"
 		
 	]	
