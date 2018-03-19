4c611f3400c2c39b70380ef8706b2dd3b86d9140
vinayak85 <vinupatil9@gmail.com>
2018-03-19 18:12:50 +0530

    Update batch_register.py

--- team/team/report/batch_register/batch_register.py
+++ team/team/report/batch_register/batch_register.py
@@ -27,25 +27,25 @@ def _execute(filters=None,  additional_query_columns=None):
 			pur_qty = frappe.db.sql("""select sum(pii.stock_qty) as 'Purchase',batch_no
 		FROM 1bd3e0294da19198.`tabPurchase Invoice` as pi LEFT JOIN 1bd3e0294da19198.`tabPurchase Invoice Item` pii
 		ON pi.name = pii.parent where pi.docstatus <> 2 and pii.batch_no='AC6080' and pi.delivery_date 
-		like concat({0} and  batch_no={1} )""".format(ss, gg), as_dict=1)
+		like concat({0} and  pii.batch_no={1} )""".format(ss, gg), as_dict=1)
 			
 			sale_qty = frappe.db.sql("""select sum(sii.stock_qty) as 'Sale',batch_no FROM 1bd3e0294da19198.`tabSales Invoice`
 		as si LEFT JOIN 1bd3e0294da19198.`tabSales Invoice Item` sii ON si.name = sii.parent where si.docstatus <> 2
 		and sii.batch_no='AC6080'  and 
 		(si.name like 'Pre-SI-0%' or si.name like 'SI-0%')
-		and si.posting_date like concat({0}) and  batch_no={1}""".format(ss, gg), as_dict=1)
+		and si.posting_date like concat({0}) and  sii.batch_no={1}""".format(ss, gg), as_dict=1)
 			
 			sample_qty = frappe.db.sql("""select sum(sii.stock_qty) as 'Sample',batch_no FROM 1bd3e0294da19198.`tabSales Invoice`
 		as si LEFT JOIN 1bd3e0294da19198.`tabSales Invoice Item` sii ON si.name = sii.parent where si.docstatus <> 2
 		and sii.batch_no='AC6080'  and 
 		(si.name like 'SS-%')
-		and si.posting_date like concat({0}) and  batch_no={1}""".format(ss, gg), as_dict=1)
+		and si.posting_date like concat({0}) and  sii.batch_no={1}""".format(ss, gg), as_dict=1)
 			
 			cn_qty = frappe.db.sql("""select sum(sii.stock_qty) as 'Credit_Note',batch_no FROM 1bd3e0294da19198.`tabSales Invoice`
 		as si LEFT JOIN 1bd3e0294da19198.`tabSales Invoice Item` sii ON si.name = sii.parent where si.docstatus <> 2
 		and sii.batch_no='AC6080'  and 
 		(si.name like 'Pre-R%' or si.name like 'SR-0%')
-		and si.posting_date like concat({0}) and  batch_no={1}""".format(ss, gg), as_dict=1)
+		and si.posting_date like concat({0}) and  sii.batch_no={1}""".format(ss, gg), as_dict=1)
 			
 			if not pur_qty[0].Purchase is None:
 				pur_qty = pur_qty[0].Purchase;
