1f84396f4ee7dad7c7bd60482ecb79ab38974569
vinayak85 <vinupatil9@gmail.com>
2018-03-20 14:42:03 +0530

    Update batch_register.js

--- team/team/report/batch_register/batch_register.js
+++ team/team/report/batch_register/batch_register.js
@@ -4,6 +4,19 @@
 
 frappe.query_reports["Batch Register"] = {
 	"filters": [
+		{
+			"fieldname":"from_date",
+			"label": __("From Date"),
+			"fieldtype": "Date",
+			"default": frappe.datetime.add_months(frappe.datetime.get_today(), -1),
+			"width": "80"
+		},
+		{
+			"fieldname":"to_date",
+			"label": __("To Date"),
+			"fieldtype": "Date",
+			"default": frappe.datetime.get_today()
+		}
 
 	]
 }
