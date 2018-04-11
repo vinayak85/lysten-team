02142f483f985fc6158424e9a246998179540fa3
vinayak85 <vinupatil9@gmail.com>
2018-04-11 17:49:05 +0530

    Update standard_tour_plan.py

--- team/team/doctype/standard_tour_plan/standard_tour_plan.py
+++ team/team/doctype/standard_tour_plan/standard_tour_plan.py
@@ -21,10 +21,18 @@ class StandardTourPlan(Document):
 @frappe.whitelist()
 #get patches details of specific user with doctor count and chemist count ,this method use in STP after buton clicl
 def get_patches_doc_and_chem_cnt(user):
-	#frappe.msgprint(_("PY ENTER"));
-	#frappe.msgprint(_(frappe.db.sql("""SELECT name,patch_name,user,user_name FROM 1bd3e0294da19198.`tabPatch master`
-	#where user like {0} and  docstatus !=2""".format("'"+user+"'"), as_dict=1)));
+	
 	return  frappe.db.sql("""SELECT name,patch_name,user,user_name FROM 1bd3e0294da19198.`tabPatch master`
 	where user like {0} and  docstatus !=2""".format("'"+user+"'"), as_dict=1);
+
+def get_patches_doc_and_chem_cnt(user):
+	doc = frappe.db.sql("""SELECT ifnull(count(name),0) as name FROM `tabDoctor Master`where user like {0} and  docstatus !=2""".format("'"+user+"'"), as_dict=1);
+	chem = frappe.db.sql("""SELECT ifnull(count(name),0) as name FROM `tabDoctor Master`where user like {0} and  docstatus !=2""".format("'"+user+"'"), as_dict=1);
+	#return c[0].name;
+	dict = {'cnt_doc': 0,
+	        'cnt_chem': 0}
+	dict['cnt_doc'] = c[0].doc;
+	dict['cnt_chem'] = c[0].chem;
+	return dict;
 	
 	
