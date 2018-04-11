e166e3529cc220f55202e0640f71e567c94eb214
vinayak85 <vinupatil9@gmail.com>
2018-04-11 18:00:40 +0530

    Update standard_tour_plan.py

--- team/team/doctype/standard_tour_plan/standard_tour_plan.py
+++ team/team/doctype/standard_tour_plan/standard_tour_plan.py
@@ -24,7 +24,7 @@ def get_patches_doc_and_chem_cnt(user):
 	
 	return  frappe.db.sql("""SELECT name,patch_name,user,user_name FROM 1bd3e0294da19198.`tabPatch master`
 	where user like {0} and  docstatus !=2""".format("'"+user+"'"), as_dict=1);
-
+@frappe.whitelist()
 def get_dr_and_chem_count_fetch(user):
 	doc = frappe.db.sql("""SELECT ifnull(count(name),0) as name FROM `tabDoctor Master`where user like {0} and  docstatus !=2""".format("'"+user+"'"), as_dict=1);
 	chem = frappe.db.sql("""SELECT ifnull(count(name),0) as name FROM `tabDoctor Master`where user like {0} and  docstatus !=2""".format("'"+user+"'"), as_dict=1);
