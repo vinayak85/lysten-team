cc54ea570709121d7865a315fd4e8cf1601d03c8
vinayak85 <vinupatil9@gmail.com>
2018-04-11 16:33:13 +0530

    Update standard_tour_plan.py

--- team/team/doctype/standard_tour_plan/standard_tour_plan.py
+++ team/team/doctype/standard_tour_plan/standard_tour_plan.py
@@ -5,6 +5,24 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
+import subprocess
+from frappe import msgprint, _ 
+import frappe.utils
+from frappe.model.document import Document
+from frappe.utils import cstr, flt, getdate, cint
+from frappe.model.naming import make_autoname
+from frappe import _
+from frappe.model.mapper import get_mapped_doc
+
 
 class StandardTourPlan(Document):
 	pass
+
+@frappe.whitelist()
+#get patches details of specific user with doctor count and chemist count ,this method use in STP after buton clicl
+def get_patches_doc_and_chem_cnt(user):
+	
+	return  frappe.db.sql("""SELECT name,patch_name,user,user_name FROM 1bd3e0294da19198.`tabPatch master`
+	where user like {0} and  docstatus !=2""".format("'"+user+"'"), as_dict=1);
+	
+	
