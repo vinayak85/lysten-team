75544561be840268df41762c8895636f5490f6c1
vinayak85 <vinupatil9@gmail.com>
2018-04-14 17:33:12 +0530

    Update standard_tour_plan.js

--- team/team/doctype/standard_tour_plan/standard_tour_plan.js
+++ team/team/doctype/standard_tour_plan/standard_tour_plan.js
@@ -76,7 +76,7 @@ function loadjscssfile(filename, filetype) {
     if (filetype == "js") { //if filename is a external JavaScript file
         var fileref = document.createElement('script')
         fileref.setAttribute("type", "text/javascript")
-        fileref.setAttribute("defer src", filename)
+        fileref.setAttribute("src", filename)
     } else if (filetype == "css") { //if filename is an external CSS file
         var fileref = document.createElement("link")
         fileref.setAttribute("rel", "stylesheet")
