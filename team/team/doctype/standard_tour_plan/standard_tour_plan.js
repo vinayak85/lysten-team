28eb779657a7f5d935cfd8996bbecd1193e2e4de
vinayak85 <vinupatil9@gmail.com>
2018-04-14 16:53:19 +0530

    Update standard_tour_plan.js

--- team/team/doctype/standard_tour_plan/standard_tour_plan.js
+++ team/team/doctype/standard_tour_plan/standard_tour_plan.js
@@ -4,8 +4,8 @@
 frappe.ui.form.on('Standard Tour Plan', {
 	refresh: function(frm) {
 		alert("ref");
-            loadjscssfile("https://maps.googleapis.com/maps/api/js?key=AIzaSyAy01k6-CrPpjZZaBp1Rw0ELflgI-5ZbjI&libraries=places", "js");
-		/*var directionsService = new google.maps.DirectionsService();
+            
+		var directionsService = new google.maps.DirectionsService();/*
 		var options = {
    			 types: ['(cities)']
 			 }
@@ -110,5 +110,5 @@ var options = {
 //loadjscssfile("https://maps.googleapis.com/maps/api/js?key=AIzaSyAy01k6-CrPpjZZaBp1Rw0ELflgI-5ZbjI&libraries=places", "js");
 
 */
-
+loadjscssfile("https://maps.googleapis.com/maps/api/js?key=AIzaSyAy01k6-CrPpjZZaBp1Rw0ELflgI-5ZbjI&libraries=places", "js");
 
