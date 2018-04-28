f9b875fd4014111182e0a5f06e628456063fb2cf
vinayak85 <vinupatil9@gmail.com>
2018-04-28 16:37:39 +0530

    Update branch_products.py

--- team/branch_products.py
+++ team/branch_products.py
@@ -1,1 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+import frappe
+from frappe import msgprint, _ 
+__version__ = '0.0.1'
+
+# this method is used for android heirachy get Hq based on user territory
+#it will featch all HQ of related to the user
+@frappe.whitelist()
+def product_list(branch):   
+    temp_flag=''
+    msg=''
+    test=''
+    if(employee == ''):
+        msg='Empty Branch...'      
+    else:
+        msg = frappe.db.sql("""select GROUP_CONCAT(name) as comma_product from `tabItem` 
+        where branch={0} group by branch""".format(branch), as_dict=1)        
+        
+    dict = {'msg': ''
+           }
+
+    dict['msg'] = msg[0].comma_product;
+    
+    return dict
 
