8f0e7606dd1a4a1e79efb105c85e864bcc1739bb
vinayak85 <vinupatil9@gmail.com>
2018-05-11 12:37:47 +0530

    Update presenty_daily_call_count_details1.py

--- team/presenty_daily_call_count_details1.py
+++ team/presenty_daily_call_count_details1.py
@@ -91,5 +91,5 @@ def missing_daily_obj_names(date,designation,branch):
   return frappe.db.sql(""" select concat(first_name,' ',last_name,' (',designation,' )')as emp_name,designation,name as email,
   ifnull(mobile_no1,'-') as mobno
   from 1bd3e0294da19198.tabUser where enabled=1 and designation in('TBM','ABM','RBM','SM','NBM') and branch={1}
-  and name not in (select user from 1bd3e0294da19198.tabObjective where select_date={0} );""".format(date,branch),as_dict=True)    
+  and name not in (select user from 1bd3e0294da19198.tabObjective where select_date={0} );""".format(date,"'"+branch+"'"),as_dict=True)    
   
