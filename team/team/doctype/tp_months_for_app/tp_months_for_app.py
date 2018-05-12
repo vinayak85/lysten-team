fa72056fc4642601346a957766f79a6650f8d3c1
vinayak85 <vinupatil9@gmail.com>
2018-05-12 17:36:22 +0530

    Update tp_months_for_app.py

--- team/team/doctype/tp_months_for_app/tp_months_for_app.py
+++ team/team/doctype/tp_months_for_app/tp_months_for_app.py
@@ -16,3 +16,13 @@ def get_user(branch):
 	
 	return frappe.db.sql("""SELECT name,full_name FROM 1bd3e0294da19198.tabUser
 where enabled=1 and branch={0}""".format(branch), as_dict=1);
+
+
+@frappe.whitelist()
+def test(test_email):
+	test_email="'"+test_email+"'";
+	tp_months_found=frappe.db.sql("""select concat(year,"-",if(month<10,concat('0',month),month))as "yyyy-mm" 
+	from `tabTP Months for App` where active=1 and name in(select parent from `tabTP Months Active user`
+	where user_id={0})""".format(test_email), as_dict=1);
+	frappe.msgprint(_(tp_months_found));
+
