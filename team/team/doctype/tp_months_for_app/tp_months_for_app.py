d9ac2e3107426644cc197d71772667263d9345ca
vinayak85 <vinupatil9@gmail.com>
2018-05-12 16:29:21 +0530

    Update tp_months_for_app.py

--- team/team/doctype/tp_months_for_app/tp_months_for_app.py
+++ team/team/doctype/tp_months_for_app/tp_months_for_app.py
@@ -8,3 +8,8 @@ from frappe.model.document import Document
 
 class TPMonthsforApp(Document):
 	pass
+
+@frappe.whitelist()
+def get_user(branch):
+	return frappe.db.sql("""SELECT name,full_name FROM 1bd3e0294da19198.tabUser
+where enabled=1 and branch={1}""".format(branch), as_dict=1);
