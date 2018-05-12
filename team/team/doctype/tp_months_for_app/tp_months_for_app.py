241cfcac59f9851527820d738628d01cebb71e31
vinayak85 <vinupatil9@gmail.com>
2018-05-12 17:54:22 +0530

    Update tp_months_for_app.py

--- team/team/doctype/tp_months_for_app/tp_months_for_app.py
+++ team/team/doctype/tp_months_for_app/tp_months_for_app.py
@@ -31,7 +31,7 @@ def test(test_email):
         	#frappe.msgprint(_(f.ym));
 		datasets1.append(f.ym);
 		ym="'"+f.ym+"'";
-		days=frappe.db.sql(""" SELECT DAY(LAST_DAY(concat({0}','-01'))) as "dd" """.format(ym), as_dict=1);
+		days=frappe.db.sql(""" SELECT DAY(LAST_DAY(concat({0},'-01'))) as "dd" """.format(ym), as_dict=1);
 		datasets1.append(days[0].dd); 
 		datasets.append(datasets1);
 		
