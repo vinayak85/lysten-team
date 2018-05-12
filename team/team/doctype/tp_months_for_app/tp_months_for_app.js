d7c269de7bcb86fcf06550a97351d45387f7d469
vinayak85 <vinupatil9@gmail.com>
2018-05-12 16:36:01 +0530

    Update tp_months_for_app.js

--- team/team/doctype/tp_months_for_app/tp_months_for_app.js
+++ team/team/doctype/tp_months_for_app/tp_months_for_app.js
@@ -16,8 +16,59 @@ frappe.ui.form.on('TP Months for App', {
 	}
 });
 test= function(frm,cdt,cdn) {
-		alert("hii");
+		frappe.call({
+			method:'team.team.doctype.tp_months_for_app.tp_months_for_app.get_user',
+			args:{
+				branch: frm.doc.branch,
+				
+			},
+			callback:function (r) {
+				
+				var active_users = $.map(frm.doc.active_users, function(d) { return d.active_users });
+				//alert(sec_items_qty );
+				for (var i=0; i< r.message.length; i++) {
+					
+					if (sec_items_qty.indexOf(r.message[i].name) === -1) {
+						
+						
+					        if(check_duplicate(frm,r.message[i].name) != false)
+					        {
+							var row = frappe.model.add_child(frm.doc, frm.fields_dict.active_users.df.options, frm.fields_dict.active_users.df.fieldname);
+							row.user_id = r.message[i].name;
+							row.user_name = r.message[i].full_name ;
+						}
+						else
+						{
+							
+						}
+					}
+						
+				
+			}
+		});
 		
 		
 };
+			    
+			
+check_duplicate= function(frm,check_user_id) {
+		//alert("hii");
+		//var sec_items_qty = $.map(frm.doc.sec_items_qty, function(d) { return d.sec_item_qty });
+		flag=true;
+		var tbl1 = frm.doc.active_users || [];
+		var strr="";
+		//var total_earn = 0; var total_ded = 0;
+	        for(var i = 0; i < tbl1.length; i++)
+		{
+			if(check_user_id==tbl1[i].user_id)
+				flag=false;;
+			
+		  //strr=strr+" ... "+ tbl1[i].item_code;
+	        }
+		return flag;
+		
+		
+	};
+			
+			
 
