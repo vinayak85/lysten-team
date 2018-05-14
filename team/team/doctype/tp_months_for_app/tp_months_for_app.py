01997101ed463c491b27e53cbcadc89418ee795d
vinayak85 <vinupatil9@gmail.com>
2018-05-14 15:31:01 +0530

    Update tp_months_for_app.py

--- team/team/doctype/tp_months_for_app/tp_months_for_app.py
+++ team/team/doctype/tp_months_for_app/tp_months_for_app.py
@@ -31,6 +31,7 @@ def test(test_email):
         	#frappe.msgprint(_(f.ym));
 		datasets1.append(f.ym);
 		ym="'"+f.ym+"'";
+		ym_="'"+f.ym+"-%'";
 		days=frappe.db.sql(""" SELECT DAY(LAST_DAY(concat({0},'-01'))) as "dd" """.format(ym), as_dict=1);
 		holiday_sun_cnt=frappe.db.sql("""SELECT count(case when description='Sunday' then 1 end)as "sunday",count(case when description!='Sunday' then 1 end)as "holiday_day" FROM 1bd3e0294da19198.`tabHoliday` where parent=(select holiday_list from tabEmployee where name =(SELECT employee_code from tabUser where name={0})) and holiday_date like ={1} """.format(test_email,ym), as_dict=1);
 		datasets1.append(days[0].dd); 
