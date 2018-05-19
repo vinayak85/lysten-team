a92ebdf05c17de9eb393b2375c13caf6c6da39a9
vinayak85 <vinupatil9@gmail.com>
2018-05-19 12:42:01 +0530

    Update tp_months_for_app.py

--- team/team/doctype/tp_months_for_app/tp_months_for_app.py
+++ team/team/doctype/tp_months_for_app/tp_months_for_app.py
@@ -20,8 +20,7 @@ where enabled=1 and branch={0}""".format(branch), as_dict=1);
 
 @frappe.whitelist()
 def test(test_email):
-	datasets = []; 
-	lst = [];
+	datasets = [];
 	test_email="'"+test_email+"'";
 	tp_months_found=frappe.db.sql("""select concat(year,"-",if(month<10,concat('0',month),month))as "ym" 
 	from `tabTP Months for App` where active=1 and name in(select parent from `tabTP Months Active user`
@@ -90,11 +89,8 @@ def test(test_email):
 		datasets1.append(tp_days_cnt[0].cnt_meeting);
 		datasets1.append(tp_days_cnt[0].cnt_leave);'''
 		datasets.append(datasets1);	
-		
-		lst.append({'fn':'b','ln':'d'});
-		lst.append({'fn':'a', 'ln':'c'})
 		pass;
-	return (lst);''', as_dict=1 datasets'''
+	return (datasets);''', as_dict=1 '''
 	
         
     
