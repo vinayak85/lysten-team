4be0c10ee4daa1492fd0065d264d37079eeaa43d
vinayak85 <vinupatil9@gmail.com>
2018-05-19 10:35:22 +0530

    Update tp_months_for_app.py

--- team/team/doctype/tp_months_for_app/tp_months_for_app.py
+++ team/team/doctype/tp_months_for_app/tp_months_for_app.py
@@ -48,7 +48,7 @@ def test(test_email):
 		and user= {1} and tp_flag=1
 		ORDER BY `tabObjective`.`name` DESC LIMIT 1""".format(ym_,test_email), as_dict=1);''', as_dict=1'''
 		dict['days']=days[0].dd;
-		frappe.msgprint(_(dict['days']));
+		frappe.msgprint(_('days:'+dict['days']));
 		datasets1.append(dict['days']); 
 		datasets1.append(holiday_sun_cnt[0].sunday);
 		datasets1.append(holiday_sun_cnt[0].holiday_day);
