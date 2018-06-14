b793cd4fe9fa087fb0d29c248f07c8d9dd40cb02
vinayak85 <vinupatil9@gmail.com>
2018-06-14 17:44:43 +0530

    Update primary_secondary_on_app.py

--- team/primary_secondary_on_app.py
+++ team/primary_secondary_on_app.py
@@ -12,7 +12,10 @@ def get_date_and_app_support(User,Stockist,FromDate,ToDate,Products):
 	where name='kasimmevekari@gmail.com' and enabled=1;""".format("'"+User+"'"), as_dict=1)
 	
 	branch=product_list(branch_p[0].branch)
-	frappe.msgprint(_(branch[0].comma_product));
+	product_list=[];
+	product_list=branch.split (',')
+	for pp in product_list:
+		frappe.msgprint(_(pp));
 	
 	'''if(designation=='ABM'):
 		msg = frappe.db.sql("""select group_concat(territory_name) from `tabTerritory` where parent_territory='Ichalkaranji(Area)'
