b802af19c3af9a825ff5d1016374a92370c4a325
vinayak85 <vinupatil9@gmail.com>
2018-07-09 12:56:25 +0530

    Update primary_secondary_on_app.py

--- team/primary_secondary_on_app.py
+++ team/primary_secondary_on_app.py
@@ -180,6 +180,7 @@ SELECT distinct stockist,full_name FROM 1bd3e0294da19198.`tabStockist For User` 
 
 @frappe.whitelist()
 def stockist_list_for_top_hierarchy(employee, designation,limit, offset):
+	frappe.msgprint(_(employee+'  '+designation))
 	if designation == "ABM":
 		return frappe.db.sql(""" SELECT distinct stockist,full_name 
 		FROM 1bd3e0294da19198.`tabStockist For User` 
