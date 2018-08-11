799dfe8c16c49394c8f8c89a167157785cdf6b7c
vinayak85 <vinupatil9@gmail.com>
2018-08-11 15:59:21 +0530

    Update test_task.py

--- team/team/doctype/test_task/test_task.py
+++ team/team/doctype/test_task/test_task.py
@@ -3,6 +3,9 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
+import datetime
+from datetime import datetime
+from pytz import timezone
 import frappe
 from frappe.model.document import Document
 from frappe import msgprint, _
@@ -14,15 +17,34 @@ class test_task(Document):
 def test_start():
 	'''INSERT INTO 1bd3e0294da19198.tabtest_task (note)
    VALUES
-   ('');'''
-	td_entry = frappe.new_doc("test_task")
+   ('');
+   
+   tabtest_task_child
+   note
+   date
+   time
+   '''
+	'''td_entry = frappe.new_doc("test_task")
 	#td_entry.name = 'test_task/' + '000001'
 	td_entry.note = 'aaaaa'
-	'''td_entry.salary_slip = ss.name'''
+	#td_entry.salary_slip = ss.name
 	#td_entry.docstatus = 1
+	td_entry.save()'''
+	
+	today_date = frappe.utils.data.get_datetime().strftime('%Y-%m-%d')
+	current_time = local_time()
+	
+	td_entry = frappe.new_doc("tabtest_task_child")
+	#td_entry.name = 'test_task/' + '000001'
+	td_entry.note = 'aaaaaaaa'
+	td_entry.date = str(today_date)
+	td_entry.time = str(current_time)
 	td_entry.save()
 	
 	#op = frappe.db.sql(""" INSERT INTO 1bd3e0294da19198.tabtest_task (note) VALUES ('aaaaa');""");
 	frappe.msgprint(_("arjun "));
 
-
+def local_time(zone='Asia/Kolkata'):
+    other_zone = timezone(zone)
+    other_zone_time = datetime.now(other_zone)
+    return other_zone_time.strftime('%T')
