1ce8d1a2033a635ed944d7ef31b8e3f89032a0e0
vinayak85 <vinupatil9@gmail.com>
2018-10-04 14:21:33 +0530

    Update form16.py

--- team/form16.py
+++ team/form16.py
@@ -71,6 +71,347 @@ def form16_allowance(employee,from_date,to_date):
 	ss.start_date between {1} and {2} and ss.status IN ('Draft', 'Submitted'); 
         """.format("'"+employee+"'","'"+from_date+"'","'"+to_date+"'"), as_dict=1)
 	
+	gross_amt_wot_exp= frappe.db.sql(""" SELECT Months,
+EMP_NAME as "EmployeeName",
+EMP as "EmployeeCode",
+GROSS  as "GROSSWithExp",
+SUM(BASIC+DA+CA+EA+HRA+SPA+OTHER+Per_Allow+wash_allow+med+E_BA+e_lta) as "GrossWotExp",
+SUM(BASIC+DA+CA+EA+HRA+SPA+OTHER+wash_allow+med+E_BA+e_lta) as "GrossWotExpPerform" 
+
+FROM
+(
+
+SELECT Months,EMP_NAME,EMP,SUM(GROSS) AS GROSS,SUM(DED) AS DED,SUM(NET) AS NET,
+SUM(BASIC) AS BASIC,SUM(DA) AS DA,SUM(CA) AS CA,SUM(EA) AS EA,SUM(HRA) AS HRA,SUM(SPA) AS SPA,
+SUM(wash_allow)AS wash_allow,SUM(Per_Allow) AS Per_Allow,sum(OTHER) AS OTHER,SUM(D_PF) AS D_PF,SUM(D_PT) AS D_PT, 
+
+SUM(med) AS med,
+SUM(E_BA) AS E_BA,
+SUM(e_lta) AS e_lta
+FROM
+
+(
+SELECT COUNT(employee_name) AS Months,SS.employee_name AS EMP_NAME,SS.Employee AS EMP,
+SUM(SS.gross_pay) AS GROSS,SUM(SS.total_deduction) AS DED,SUM(SS.rounded_total) AS NET,
+0 AS BASIC,
+0 AS DA,
+0 AS CA,
+0 AS EA,
+0 AS HRA,
+0 AS SPA,
+0 AS wash_allow,
+0 AS Per_Allow,
+0 AS OTHER,
+0 AS D_PF,
+0 AS D_PT,
+0 AS med,
+0 AS E_BA,
+0 AS e_lta
+ FROM 1bd3e0294da19198.`tabSalary Slip` AS SS
+ WHERE SS.start_date >={1} AND SS.start_date <={2}  and status IN ('Draft', 'Submitted')
+GROUP BY  SS.Employee,SS.employee_name
+UNION  ALL
+SELECT 0 AS Months,SS.employee_name AS EMP_NAME,SS.Employee AS EMP,0 AS GROSS,0 AS DED,0 AS NET,
+SUM(SD.AMOUNT) AS BASIC,
+0 AS DA,
+0 AS CA,
+0 AS EA,
+0 AS HRA,
+0 AS SPA,
+0 AS wash_allow,
+0 AS Per_Allow,
+0 AS OTHER,
+0 AS D_PF,
+0 AS D_PT,
+0 AS med,
+0 AS E_BA,
+0 AS e_lta
+ FROM 1bd3e0294da19198.`tabSalary Slip` AS SS
+  LEFT   JOIN 1bd3e0294da19198.`tabSalary Detail` AS SD ON SD.PARENT = SS.NAME
+  WHERE SD.ABBR='E_B'
+  AND SS.start_date >={1} AND SS.start_date <={2}  and status IN ('Draft', 'Submitted')
+GROUP BY  SS.Employee,SS.employee_name
+UNION  ALL
+SELECT 0 AS Months,SS.employee_name AS EMP_NAME,SS.Employee AS EMP,0 AS GROSS,0 AS DED,0 AS NET,
+0 AS BASIC,
+SUM(SD.AMOUNT) AS DA,
+0 AS CA,
+0 AS EA,
+0 AS HRA,
+0 AS SPA,
+0 AS wash_allow,
+0 AS Per_Allow,
+0 AS OTHER,
+0 AS D_PF,
+0 AS D_PT,
+0 AS med,
+0 AS E_BA,
+0 AS e_lta
+ FROM 1bd3e0294da19198.`tabSalary Slip` AS SS
+  LEFT   JOIN 1bd3e0294da19198.`tabSalary Detail` AS SD ON SD.PARENT = SS.NAME
+  WHERE SD.ABBR='E_DA'
+  AND SS.start_date >={1} AND SS.start_date <={2}  and status IN ('Draft', 'Submitted')
+GROUP BY  SS.Employee,SS.employee_name
+UNION  ALL
+SELECT 0 AS Months,SS.employee_name AS EMP_NAME,SS.Employee AS EMP,0 AS GROSS,0 AS DED,0 AS NET,
+0 AS BASIC,
+0 AS DA,
+SUM(SD.AMOUNT) AS CA,
+0 AS EA,
+0 AS HRA,
+0 AS SPA,
+0 AS wash_allow,
+0 AS Per_Allow,
+0 AS OTHER,
+0 AS D_PF,
+0 AS D_PT,
+0 AS med,
+0 AS E_BA,
+0 AS e_lta
+ FROM 1bd3e0294da19198.`tabSalary Slip` AS SS
+  LEFT   JOIN 1bd3e0294da19198.`tabSalary Detail` AS SD ON SD.PARENT = SS.NAME
+  WHERE SD.ABBR='E_CA'
+  AND SS.start_date >={1} AND SS.start_date <={2}  and status IN ('Draft', 'Submitted')
+GROUP BY  SS.Employee,SS.employee_name
+UNION  ALL
+SELECT 0 AS Months,SS.employee_name AS EMP_NAME,SS.Employee AS EMP,0 AS GROSS,0 AS DED,0 AS NET,
+0 AS BASIC,
+0 AS DA,
+0 CA,
+SUM(SD.AMOUNT) AS EA,
+0 AS HRA,
+0 AS SPA,
+0 AS wash_allow,
+0 AS Per_Allow,
+0 AS OTHER,
+0 AS D_PF,
+0 AS D_PT,
+0 AS med,
+0 AS E_BA,
+0 AS e_lta
+ FROM 1bd3e0294da19198.`tabSalary Slip` AS SS
+  LEFT   JOIN 1bd3e0294da19198.`tabSalary Detail` AS SD ON SD.PARENT = SS.NAME
+  WHERE SD.ABBR='E_EA'
+  AND SS.start_date >={1} AND SS.start_date <={2}  and status IN ('Draft', 'Submitted')
+GROUP BY  SS.Employee,SS.employee_name
+UNION  ALL
+SELECT 0 AS Months,SS.employee_name AS EMP_NAME,SS.Employee AS EMP,0 AS GROSS,0 AS DED,0 AS NET,
+0 AS BASIC,
+0 AS DA,
+0 AS CA,
+0 AS EA,
+SUM(SD.AMOUNT) AS HRA,
+0 AS SPA,
+0 AS wash_allow,
+0 AS Per_Allow,
+0 AS OTHER,
+0 AS D_PF,
+0 AS D_PT,
+0 AS med,
+0 AS E_BA,
+0 AS e_lta
+ FROM 1bd3e0294da19198.`tabSalary Slip` AS SS
+  LEFT   JOIN 1bd3e0294da19198.`tabSalary Detail` AS SD ON SD.PARENT = SS.NAME
+  WHERE SD.ABBR='E_HRA'
+  AND SS.start_date >={1} AND SS.start_date <={2}  and status IN ('Draft', 'Submitted')
+GROUP BY  SS.Employee,SS.employee_name
+UNION  ALL
+SELECT 0 AS Months,SS.employee_name AS EMP_NAME,SS.Employee AS EMP,0 AS GROSS,0 AS DED,0 AS NET,
+0 AS BASIC,
+0 AS DA,
+0 AS CA,
+0 AS EA,
+0 AS HRA,
+SUM(SD.AMOUNT) AS SPA,
+0 AS wash_allow,
+0 AS Per_Allow,
+0 AS OTHER,
+0 AS D_PF,
+0 AS D_PT,
+0 AS med,
+0 AS E_BA,
+0 AS e_lta
+ FROM 1bd3e0294da19198.`tabSalary Slip` AS SS
+  LEFT   JOIN 1bd3e0294da19198.`tabSalary Detail` AS SD ON SD.PARENT = SS.NAME
+  WHERE SD.ABBR='E_SA'
+  AND SS.start_date >={1} AND SS.start_date <={2}  and status IN ('Draft', 'Submitted')
+GROUP BY  SS.Employee,SS.employee_name
+UNION  ALL
+SELECT 0 AS Months,SS.employee_name AS EMP_NAME,SS.Employee AS EMP,0 AS GROSS,0 AS DED,0 AS NET,
+0 AS BASIC,
+0 AS DA,
+0 AS CA,
+0 AS EA,
+0 AS HRA,
+0 AS SPA,
+SUM(SD.AMOUNT) AS wash_allow,
+0 AS Per_Allow,
+0 AS OTHER,
+0 AS D_PF,
+0 AS D_PT,
+0 AS med,
+0 AS E_BA,
+0 AS e_lta
+ FROM 1bd3e0294da19198.`tabSalary Slip` AS SS
+  LEFT   JOIN 1bd3e0294da19198.`tabSalary Detail` AS SD ON SD.PARENT = SS.NAME
+  WHERE SD.ABBR='wash_allow'
+  AND SS.start_date >={1} AND SS.start_date <={2}  and status IN ('Draft', 'Submitted')
+GROUP BY  SS.Employee,SS.employee_name
+UNION  ALL
+SELECT 0 AS Months,SS.employee_name AS EMP_NAME,SS.Employee AS EMP,0 AS GROSS,0 AS DED,0 AS NET,
+0 AS BASIC,
+0 AS DA,
+0 AS CA,
+0 AS EA,
+0 AS HRA,
+0 AS SPA,
+0 AS wash_allow,
+SUM(SD.AMOUNT) AS Per_Allow,
+0 AS OTHER,
+0 AS D_PF,
+0 AS D_PT,
+0 AS med,
+0 AS E_BA,
+0 AS e_lta
+ FROM 1bd3e0294da19198.`tabSalary Slip` AS SS
+  LEFT   JOIN 1bd3e0294da19198.`tabSalary Detail` AS SD ON SD.PARENT = SS.NAME
+  WHERE SD.ABBR='Per_Allow'
+  AND SS.start_date >={1} AND SS.start_date <={2}  and status IN ('Draft', 'Submitted')
+GROUP BY  SS.Employee,SS.employee_name
+UNION  ALL
+SELECT 0 AS Months,SS.employee_name AS EMP_NAME,SS.Employee AS EMP,0 AS GROSS,0 AS DED,0 AS NET,
+0 AS BASIC,
+0 AS DA,
+0 AS CA,
+0 AS EA,
+0 AS HRA,
+0 AS SPA,
+0 AS wash_allow,
+0 AS Per_Allow,
+SUM(SD.AMOUNT) AS OTHER,
+0 AS D_PF,
+0 AS D_PT,
+0 AS med,
+0 AS E_BA,
+0 AS e_lta
+ FROM 1bd3e0294da19198.`tabSalary Slip` AS SS
+  LEFT   JOIN 1bd3e0294da19198.`tabSalary Detail` AS SD ON SD.PARENT = SS.NAME
+  WHERE SD.ABBR='E_OA'
+  AND SS.start_date >={1} AND SS.start_date <={2}  and status IN ('Draft', 'Submitted')
+GROUP BY  SS.Employee,SS.employee_name
+UNION  ALL
+SELECT 0 AS Months,SS.employee_name AS EMP_NAME,SS.Employee AS EMP,0 AS GROSS,0 AS DED,0 AS NET,
+0 AS BASIC,
+0 AS DA,
+0 AS CA,
+0 AS EA,
+0 AS HRA,
+0 AS SPA,
+0 AS wash_allow,
+0 AS Per_Allow,
+0 AS OTHER,
+SUM(SD.AMOUNT) AS D_PF,
+0 AS D_PT,
+0 AS med,
+0 AS E_BA,
+0 AS e_lta
+ FROM 1bd3e0294da19198.`tabSalary Slip` AS SS
+  LEFT   JOIN 1bd3e0294da19198.`tabSalary Detail` AS SD ON SD.PARENT = SS.NAME
+  WHERE SD.ABBR='D_PF'
+  AND SS.start_date >={1} AND SS.start_date <={2}  and status IN ('Draft', 'Submitted')
+GROUP BY  SS.Employee,SS.employee_name
+UNION  ALL
+SELECT 0 AS Months,SS.employee_name AS EMP_NAME,SS.Employee AS EMP,0 AS GROSS,0 AS DED,0 AS NET,
+0 AS BASIC,
+0 AS DA,
+0 AS CA,
+0 AS EA,
+0 AS HRA,
+0 AS SPA,
+0 AS wash_allow,
+0 AS Per_Allow,
+0 AS OTHER,
+0 AS D_PF,
+SUM(SD.AMOUNT) AS D_PT,
+0 AS med,
+0 AS E_BA,
+0 AS e_lta
+ FROM 1bd3e0294da19198.`tabSalary Slip` AS SS
+  LEFT   JOIN 1bd3e0294da19198.`tabSalary Detail` AS SD ON SD.PARENT = SS.NAME
+  WHERE SD.ABBR='D_PT'
+  AND SS.start_date >={1} AND SS.start_date <={2}  and status IN ('Draft', 'Submitted')
+GROUP BY  SS.Employee,SS.employee_name
+UNION  ALL
+SELECT 0 AS Months,SS.employee_name AS EMP_NAME,SS.Employee AS EMP,0 AS GROSS,0 AS DED,0 AS NET,
+0 AS BASIC,
+0 AS DA,
+0 AS CA,
+0 AS EA,
+0 AS HRA,
+0 AS SPA,
+0 AS wash_allow,
+0 AS Per_Allow,
+0 AS OTHER,
+0 AS D_PF,
+0 AS D_PT,
+SUM(SD.AMOUNT) AS med,
+0 AS E_BA,
+0 AS e_lta
+ FROM 1bd3e0294da19198.`tabSalary Slip` AS SS
+  LEFT   JOIN 1bd3e0294da19198.`tabSalary Detail` AS SD ON SD.PARENT = SS.NAME
+  WHERE SD.ABBR='med'
+  AND SS.start_date >={1} AND SS.start_date <={2}  and status IN ('Draft', 'Submitted')
+GROUP BY  SS.Employee,SS.employee_name
+UNION  ALL
+SELECT 0 AS Months,SS.employee_name AS EMP_NAME,SS.Employee AS EMP,0 AS GROSS,0 AS DED,0 AS NET,
+0 AS BASIC,
+0 AS DA,
+0 AS CA,
+0 AS EA,
+0 AS HRA,
+0 AS SPA,
+0 AS wash_allow,
+0 AS Per_Allow,
+0 AS OTHER,
+0 AS D_PF,
+0 AS D_PT,
+0 AS med,
+SUM(SD.AMOUNT) AS E_BA,
+0 AS e_lta
+ FROM 1bd3e0294da19198.`tabSalary Slip` AS SS
+  LEFT   JOIN 1bd3e0294da19198.`tabSalary Detail` AS SD ON SD.PARENT = SS.NAME
+  WHERE SD.ABBR='E_BA'
+  AND SS.start_date >={1} AND SS.start_date <={2}  and status IN ('Draft', 'Submitted')
+GROUP BY  SS.Employee,SS.employee_name
+UNION  ALL
+SELECT 0 AS Months,SS.employee_name AS EMP_NAME,SS.Employee AS EMP,0 AS GROSS,0 AS DED,0 AS NET,
+0 AS BASIC,
+0 AS DA,
+0 AS CA,
+0 AS EA,
+0 AS HRA,
+0 AS SPA,
+0 AS wash_allow,
+0 AS Per_Allow,
+0 AS OTHER,
+0 AS D_PF,
+0 AS D_PT,
+0 AS med,
+0 AS E_BA,
+SUM(SD.AMOUNT) AS e_lta
+ FROM 1bd3e0294da19198.`tabSalary Slip` AS SS
+  LEFT   JOIN 1bd3e0294da19198.`tabSalary Detail` AS SD ON SD.PARENT = SS.NAME
+  WHERE SD.ABBR='e_lta'
+  AND SS.start_date >={1} AND SS.start_date <={2}  and status IN ('Draft', 'Submitted')
+GROUP BY  SS.Employee,SS.employee_name
+
+) AS TEMP
+GROUP BY  EMP_NAME,EMP
+
+ ) AS TAXBLE where EMP={0} GROUP BY  EMP_NAME,EMP
+	""".format("'"+employee+"'","'"+from_date+"'","'"+to_date+"'"), as_dict=1)
+		
+	'''
 	gross_amt_wot_exp= frappe.db.sql("""SELECT Months,EMP_NAME as "EmployeeName",EMP as "EmployeeCode",
 GROSS  as "GROSSWithExp",
 SUM(BASIC+DA+CA+EA+HRA+SPA+OTHER+Per_Allow+wash_allow) as "GrossWotExp",
@@ -321,7 +662,7 @@ GROUP BY  SS.Employee,SS.employee_name
 ) AS TEMP
 GROUP BY  EMP_NAME,EMP ) AS TAXBLE where EMP={0} GROUP BY  EMP_NAME,EMP
         """.format("'"+employee+"'","'"+from_date+"'","'"+to_date+"'"), as_dict=1)	
-	
+'''	
      
     dict = {'paid_month_count':'',
 	    'gross_amt':'',
