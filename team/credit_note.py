4f02381c7c9184012336876b74286890c098f4fa
vinayak85 <vinupatil9@gmail.com>
2019-03-19 00:08:28 +0530

    Update credit_note.py

--- team/credit_note.py
+++ team/credit_note.py
@@ -6,4 +6,7 @@ __version__ = '0.0.1'
 
 @frappe.whitelist()
 def test(against_inv,sr):
-  frappe.msgprint(_(against_inv + "," + sr))
+  #frappe.msgprint(_(against_inv + "," + sr))
+  #posting_date
+  return frappe.db.sql(""" select posting_date from 1bd3e0294da19198.`tabSales Invoice`
+  where `tabSales Invoice`.`enabled`=1 and `name`={0}""".format(sr), as_dict=1)
