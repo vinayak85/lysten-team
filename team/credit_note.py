70e5322c9dcd1194b411d9367767bbad1254ae0d
vinayak85 <vinupatil9@gmail.com>
2019-03-19 18:40:04 +0530

    Update credit_note.py

--- team/credit_note.py
+++ team/credit_note.py
@@ -11,6 +11,10 @@ def test(against_inv,sr):
   ret_date='';
   ret_date= frappe.db.sql(""" select posting_date  from 1bd3e0294da19198.`tabSales Invoice`
   where `tabSales Invoice`.`docstatus`< 2 and `name`={0}""".format(sr), as_dict=1)
+  if len(ret_date) > 0:
+    ret_date=ret_date[0].posting_date
+   else:
+    ret_date=''
   dict = {'count': ''}
-  dict['count'] = ret_date[0].posting_date;
+  dict['count'] = ret_date;
   return dict;
