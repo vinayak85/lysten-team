206dfe6b0ecde06bdfa2cc6fa1ec7a43d2ca1556
vinayak85 <vinupatil9@gmail.com>
2019-03-21 15:56:05 +0000

    comment

---
+++ team/team/doctype/crosscheck_sales_return/crosscheck_sales_return.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2019, Vinayak Patil and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('CrossCheck Sales Return', {
+	refresh: function(frm) {
+
+	}
+});
