4a7b37470b97e580002fd1636eaf17138ec5fd88
vinayak85 <vinupatil9@gmail.com>
2019-03-21 16:47:41 +0530

    Update credit_note.py

--- team/credit_note.py
+++ team/credit_note.py
@@ -12,27 +12,53 @@ def test(against_inv,sr):
   sr="'"+sr+"'"
   ret_date='';
   transporter_id='';
+  transporter_name='';
+  transporter_address='';
+  transporter_gst_no='';
+  transporter_state_code='';
+  transporter_pan_no='';
   note=''
   if_existed=''
-  qry= frappe.db.sql(""" select posting_date,note,transporter_id,transporter_address ,transporter_gst_no,transporter_state_code,transporter_pan_no  from 1bd3e0294da19198.`tabSales Invoice`
+  new_crn=''
+  qry= frappe.db.sql(""" select posting_date,note,transporter_id,transporter_name,transporter_address ,transporter_gst_no,
+  transporter_state_code,transporter_pan_no  from 1bd3e0294da19198.`tabSales Invoice`
   where `tabSales Invoice`.`docstatus`< 2 and `name`={0}""".format(sr), as_dict=1)
   if len(qry) > 0:
     ret_date = qry[0].posting_date;
     transporter_id= qry[0].transporter_id;
+    transporter_name= qry[0].transporter_name;
+    transporter_address= qry[0].transporter_address;
+    transporter_gst_no= qry[0].transporter_gst_no;
+    transporter_state_code= qry[0].transporter_state_code;
+    transporter_pan_no= qry[0].transporter_pan_no;
     note= qry[0].note;
     pass
   else:
     ret_date = ''
     transporter_id= '';
+    transporter_name= '';
+    transporter_address= '';
+    transporter_gst_no= '';
+    transporter_state_code= '';
+    transporter_pan_no= '';
     note= '';
     pass
   
-  if_existed= frappe.db.sql(""" select name    from 1bd3e0294da19198.`tabSales Invoice` where return_against={0} and ref_return={1}""".format(against_inv,sr), as_dict=1)
-  if len(if_existed) > 0:
-    if_existed = qry[0].name;
+  qry1= frappe.db.sql(""" select name    from 1bd3e0294da19198.`tabSales Invoice` where return_against={0} and ref_return={1}""".format(against_inv,sr), as_dict=1)
+  if len(qry1) > 0:
+    if_existed = qry1[0].name;
     pass
   else:
-    ret_date = ''
+    if_existed = 'no'
+    pass
+  
+  qry2= frappe.db.sql(""" select new_credit_note_number    from 1bd3e0294da19198.`tabSales Invoice`
+where  ref_return={0}""".format,sr), as_dict=1)
+  if len(qry2) > 0:
+    new_crn = qry1[0].name;
+    pass
+  else:
+    new_crn = sr+'-1'
     pass
   
   
@@ -40,10 +66,22 @@ def test(against_inv,sr):
 where parent={0} and against_invoice_={1}""".format(sr,against_inv), as_dict=1)
   dict = {'ret_date': '',
           'transporter_id': '',
+          'transporter_name'= '',
+          'transporter_address'= '',
+          'transporter_gst_no'= '',
+          'transporter_state_code'= '',
+          'transporter_pan_no'= '',
           'note': '',
-          'sr_items':''}
+          'sr_items':'',
+          'if_existed':''}
   dict['ret_date'] = ret_date;
   dict['transporter_id'] = transporter_id;
+  dict['transporter_name'] = transporter_name;
+  dict['transporter_address'] = transporter_address;
+  dict['transporter_gst_no'] = transporter_gst_no;
+  dict['transporter_state_code'] = transporter_state_code;
+  dict['transporter_pan_no'] = transporter_pan_no;
   dict['note'] = note;
   dict['sr_items'] = sr_items;
+  dict['if_existed'] = if_existed;
   return dict;
